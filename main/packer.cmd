ade6b97cbd3513811919e502e4086e7aa2d7e498
shypike <>
2007-11-04 17:54:34 +0000

    Simple 7zip based packing script for Windows.

---
+++ main/packer.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+set prod=SABnzbd-0.2.8test
+
+ren dist %prod%
+if errorlevel 1 goto error
+
+"c:\Program Files\7-Zip\7z.exe" a -r -w -sfx7z.sfx %prod%.exe  %prod%
+if errorlevel 1 goto error
+
+ren %prod% dist
+if errorlevel 1 goto error
+
+goto end
+
+
+:error
+echo Sorry, something went wrong
+
+:end
