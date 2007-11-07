12b7205af5e92497cf2c2936cdbd26e06a9abb81
shypike <>
2007-11-07 21:24:34 +0000

    Update various files with version number 0.2.8rc2. Remove SABnzbd.ini.sample.
    CHANGELOG.txt updated.

--- main/Sample-PostProc.cmd
+++ main/Sample-PostProc.cmd
@@ -1,11 +1,11 @@
 @echo off
-rem Example of a post processing script for SABnzbd 0.2.7
+rem Example of a post processing script for SABnzbd 0.2.8
 
 echo.
 echo Running in directory "%~d0%~p0"
 echo.
-echo The first parameter is %1
-echo The second parameter is %2
+echo The first parameter (result-dir) is %1
+echo The second parameter (nzb-name) is %2
 echo.
 
 
--- main/sabnzbd/__init__.py
+++ main/sabnzbd/__init__.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-__version__ = "0.2.8test"
+__version__ = "0.2.8rc2"
 __configversion__ = 16
 __queueversion__ = 5
 __NAME__ = "sabnzbd"
