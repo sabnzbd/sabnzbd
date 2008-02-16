5f5a1057f46a81dc213f6420b60a4dfacfba53b3
swi-tch <>
2008-02-16 16:05:58 +0000

    SMPL: Changed javascript libraries for their compressed versions

--- main/interfaces/smpl/templates/static/MochiKit/__package__.js
+++ main/interfaces/smpl/templates/static/MochiKit/__package__.js
@@ -1,18 +1,2 @@
-dojo.kwCompoundRequire({
-    "common": [
-        "MochiKit.Base",
-        "MochiKit.Iter",
-        "MochiKit.Logging",
-        "MochiKit.DateTime",
-        "MochiKit.Format",
-        "MochiKit.Async",
-        "MochiKit.DOM",
-        "MochiKit.Style",
-        "MochiKit.LoggingPane",
-        "MochiKit.Color",
-        "MochiKit.Signal",
-        "MochiKit.Position",
-        "MochiKit.Visual"
-    ]
-});
-dojo.provide("MochiKit.*");
+dojo.hostenv.conditionalLoadModule({"common": ["MochiKit.MochiKit"]});
+dojo.hostenv.moduleLoaded("MochiKit.*");
