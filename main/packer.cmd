6e9dbf4be39cca71f6530d3207fce9f48ba63a67
shypike <>
2008-04-06 17:09:09 +0000

    Update last files to 0.4.0Beta1

--- main/packer.cmd
+++ main/packer.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set VERSION=0.4.0X
+set VERSION=0.4.0Beta1
 
 
 rem ***************************
--- main/sabnzbd/__init__.py
+++ main/sabnzbd/__init__.py
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-__version__ = "0.4.0X"
+__version__ = "0.4.0Beta1"
 __configversion__ = 18
 __queueversion__ = 6
 __NAME__ = "sabnzbd"
