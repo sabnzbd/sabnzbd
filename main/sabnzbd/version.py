ec0786bcae54aa419f2a15b5f849fc3900e8f950
shypike <>
2008-05-07 18:08:03 +0000

    version.py should contain the branch name (so "trunk" for now). Also add some explanation.

--- main/sabnzbd/version.py
+++ main/sabnzbd/version.py
@@ -1,2 +1,8 @@
-__version__ = "undefined"
+# This file will be patched by setup.py
+# The __version__ should be set to the branch name
+# (e.g. "trunk" or "0.4.x")
+
+# You MUST use double quotes (so " and not ')
+
+__version__ = "trunk"
 __baseline__ = "unknown"
