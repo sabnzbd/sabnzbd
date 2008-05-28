4193096a10f947fd6635a1c81d74175d407f19bb
swi-tch <>
2008-05-28 19:47:38 +0000

    Default + SMPL: Added links to forum and wiki on the main pages.

---
+++ main/interfaces/Default/templates/static/javascript/dropdown.js
@@ -0,0 +1,12 @@
+sfHover = function() {
+	var sfEls = document.getElementById("nav").getElementsByTagName("LI");
+	for (var i=0; i<sfEls.length; i++) {
+		sfEls[i].onmouseover=function() {
+			this.className+=" sfhover";
+		}
+		sfEls[i].onmouseout=function() {
+			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+		}
+	}
+}
+if (window.attachEvent) window.attachEvent("onload", sfHover);
