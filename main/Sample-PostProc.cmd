b74a74cf66182f2a7dbbfddeca5cea5c842a7817
shypike <>
2008-05-30 15:27:12 +0000

    Script samples speak still of newzbin-category: should be category.

--- main/Sample-PostProc.cmd
+++ main/Sample-PostProc.cmd
@@ -8,7 +8,7 @@ echo The first parameter (result-dir) = %1
 echo The second parameter (nzb-name)  = %2
 echo The third parameter (nice name)  = %3
 echo The fourth parameter (newzbin #) = %4
-echo The fifth parameter (newzbin-category) = %5
+echo The fifth parameter (category)   = %5
 echo.
 
 
