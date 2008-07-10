d72cbcdec2275d66fd724d2b97a0c7f9c5891133
shypike <>
2008-07-10 14:56:03 +0000

    Clear up license in headers. Merged #49 to trunk.

--- main/sabnzbd/articlecache.py
+++ main/sabnzbd/articlecache.py
@@ -2,9 +2,8 @@
 # Copyright 2008 The SABnzbd-Team <team@sabnzbd.org>
 #
 # This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# modify it under the terms of the GNU General Public License V2
+# as published by the Free Software Foundation.
 # 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
