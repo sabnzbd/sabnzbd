a13db56b6f56a043770425e2d9b6f6a9f55e0648
pairofdimes <>
2008-07-13 04:52:22 +0000

    plush | drastically simplified config code through use of template includes

---
+++ main/interfaces/Plush/templates/static/javascripts/plush.config.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	
+	$('.config_menu').corner("round tl bl");
+	$('#config_container').corner("round");
+	$('#help').corner("round");
+	
+	switch(config_pane) {
+		case 'Connections':
+			$('#test_email').corner("round");
+			$('#force_disconnect').corner("round");
+			break;
+		case 'Folders':
+			previewtv();
+		case 'General':
+		case 'Switches':
+		case 'Email':
+		case 'Newzbin':
+			$('#save').corner("round");
+			break;
+	};
+
+});
