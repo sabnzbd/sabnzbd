9b0726553e18130c5d687c1ba166f32814bfaf40
pairofdimes <>
2009-01-25 17:34:03 +0000

    plush | relocated email test button to email config; other misc tweaks

--- main/interfaces/Plush/templates/static/javascripts/config.js
+++ main/interfaces/Plush/templates/static/javascripts/config.js
@@ -68,23 +68,13 @@ jQuery(document).ready(function($){
     
     $('.config_menu').corner("round tl bl");
     $('#config_container').corner("round");
-    $('#help').corner("round");
+    $('.rounded').corner("round");
     
     switch(config_pane) {
-        case 'Connections':
-            $('#test_email').corner("round");
-            $('#force_disconnect').corner("round");
-            break;
         case 'Sorting':
             previewtv();
             previewmovie();
             previewdate();
-        case 'General':
-        case 'Folders':
-        case 'Switches':
-        case 'Email':
-        case 'Index Sites':
-            $('#save').corner("round");
             break;
     };
 
