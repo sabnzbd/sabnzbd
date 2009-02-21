a14760044f79530c5e404265e0da7614d7af74ca
shypike <>
2009-02-21 16:59:11 +0000

    Prevent warning about "from sabnzbd.utils.certgen import *" in misc.py. Just name the imported functions explicitly.

--- main/sabnzbd/misc.py
+++ main/sabnzbd/misc.py
@@ -852,7 +852,7 @@ def get_ext(filename):
         return os.path.splitext(filename)[1].lower()
     except:
         return ''
-    
+
 def get_filename(path):
     try:
         return os.path.split(path)[1]
@@ -946,27 +946,27 @@ except AttributeError:
             return disk_size / GIGI
         except:
             return 0.0
-        
-        
+
+
 def create_https_certificates(ssl_cert, ssl_key):
     try:
         from OpenSSL import crypto
-        from sabnzbd.utils.certgen import *
+        from sabnzbd.utils.certgen import createKeyPair, createCertRequest, createCertificate
     except:
         logging.warning('pyopenssl module missing, please install for https access')
         return False
-    
+
     # Create the CA Certificate
     cakey = createKeyPair(TYPE_RSA, 1024)
     careq = createCertRequest(cakey, CN='Certificate Authority')
     cacert = createCertificate(careq, (careq, cakey), serial, (0, 60*60*24*365*10)) # ten years
-    
+
     fname = 'server'
     cname = 'SABnzbd'
     pkey = createKeyPair(TYPE_RSA, 1024)
     req = createCertRequest(pkey, CN=cname)
     cert = createCertificate(req, (cacert, cakey), serial, (0, 60*60*24*365*10)) # ten years
-    
+
     # Save the key and certificate to disk
     try:
         open(ssl_key, 'w').write(crypto.dump_privatekey(crypto.FILETYPE_PEM, pkey))
@@ -975,5 +975,5 @@ def create_https_certificates(ssl_cert, ssl_key):
         logging.error('Error creating SSL key and certificate')
         logging.debug("Traceback: ", exc_info = True)
         return False
-    
+
     return True
