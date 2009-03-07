3b782fb59180709bba1401b79182493d3c6ae0a7
shypike <>
2009-03-07 15:51:34 +0000

    Fixed a test case in listquote.py.

--- main/sabnzbd/utils/listquote.py
+++ main/sabnzbd/utils/listquote.py
@@ -20,9 +20,9 @@
 # E-mail fuzzyman@voidspace.org.uk
 
 """
-Having written modules to handle turning a string representation of a list back 
-into a list (including nested lists) and also a very simple CSV parser, I 
-realised I needed a more solid set of functions for handling lists (comma 
+Having written modules to handle turning a string representation of a list back
+into a list (including nested lists) and also a very simple CSV parser, I
+realised I needed a more solid set of functions for handling lists (comma
 delimited lines) and quoting/unquoting elements of lists.
 
 The test stuff provides useful examples of how the functions work.
@@ -87,26 +87,26 @@ class CSVError(ListQuoteError):
 
 def elem_quote(member, nonquote=True, stringify=False, encoding=None):
     """
-    Simple method to add the most appropriate quote to an element - either single 
+    Simple method to add the most appropriate quote to an element - either single
     quotes or double quotes.
-    
-    If member contains ``\n`` a ``QuoteError`` is raised - multiline values 
+
+    If member contains ``\n`` a ``QuoteError`` is raised - multiline values
     can't be quoted by elem_quote.
-    
-    If ``nonquote`` is set to ``True`` (the default), then if member contains none 
+
+    If ``nonquote`` is set to ``True`` (the default), then if member contains none
     of ``'," []()#;`` then it isn't quoted at all.
-    
-    If member contains both single quotes *and* double quotes then all double 
-    quotes (``"``) will be escaped as ``&mjf-quot;`` and member will then be quoted 
+
+    If member contains both single quotes *and* double quotes then all double
+    quotes (``"``) will be escaped as ``&mjf-quot;`` and member will then be quoted
     with double quotes.
-    
-    If ``stringify`` is set to ``True`` (the default is ``False``) then non string 
-    (unicode or byte-string) values will be first converted to strings using the 
+
+    If ``stringify`` is set to ``True`` (the default is ``False``) then non string
+    (unicode or byte-string) values will be first converted to strings using the
     ``str`` function. Otherwise elem_quote raises a ``TypeError``.
-    
-    If ``encoding`` is not ``None`` and member is a byte string, then it will be 
+
+    If ``encoding`` is not ``None`` and member is a byte string, then it will be
     decoded into unicode using this encoding.
-    
+
     >>> elem_quote('hello')
     'hello'
     >>> elem_quote('hello', nonquote=False)
@@ -140,7 +140,7 @@ def elem_quote(member, nonquote=True, stringify=False, encoding=None):
     #
     if nonquote and badchars.match(member) is not None:
         return member
-    # this ordering of tests determines which quote character will be used in 
+    # this ordering of tests determines which quote character will be used in
     # preference - here we have \" first...
     elif member.find('"') == -1:
         return '"%s"' % member
@@ -150,25 +150,25 @@ def elem_quote(member, nonquote=True, stringify=False, encoding=None):
     else:
         raise QuoteError('Value can\'t be quoted : "%s"' % member)
 
-def unquote(inline, fullquote=True, retain=False): 
+def unquote(inline, fullquote=True, retain=False):
     """
     Unquote a value.
-    
+
     If the value isn't quoted it returns the value.
-    
+
     If the value is badly quoted it raises ``UnQuoteError``.
-    
-    If retain is ``True`` (default is ``False``) then the quotes are left 
-    around the value (but leading or trailing whitespace will have been 
+
+    If retain is ``True`` (default is ``False``) then the quotes are left
+    around the value (but leading or trailing whitespace will have been
     removed).
-    
-    If fullquote is ``False`` (default is ``True``) then unquote will only 
-    unquote the first part of the ``inline``. If there is anything after the 
-    quoted element, this will be returned as well (instead of raising an 
+
+    If fullquote is ``False`` (default is ``True``) then unquote will only
+    unquote the first part of the ``inline``. If there is anything after the
+    quoted element, this will be returned as well (instead of raising an
     error).
-    
+
     In this case the return value is ``(value, rest)``.
-    
+
     >>> unquote('hello')
     'hello'
     >>> unquote('"hello"')
@@ -190,7 +190,7 @@ def unquote(inline, fullquote=True, retain=False):
             return inline
         else:
             # badly quoted
-            raise UnQuoteError('Value is badly quoted: "%s"' % inline) 
+            raise UnQuoteError('Value is badly quoted: "%s"' % inline)
     quoted, rest = mat.groups()
     if fullquote and rest.strip():
         # badly quoted
@@ -204,16 +204,16 @@ def unquote(inline, fullquote=True, retain=False):
 
 def quote_escape(value, lf='&mjf-lf;', quot='&mjf-quot;'):
     """
-    Escape a string so that it can safely be quoted. You should use this if the 
-    value to be quoted *may* contain line-feeds or both single quotes and double 
+    Escape a string so that it can safely be quoted. You should use this if the
+    value to be quoted *may* contain line-feeds or both single quotes and double
     quotes.
-    
-    If the value contains ``\n`` then it will be escaped using ``lf``. By 
+
+    If the value contains ``\n`` then it will be escaped using ``lf``. By
     default this is ``&mjf-lf;``.
-    
-    If the value contains single quotes *and* double quotes, then all double 
+
+    If the value contains single quotes *and* double quotes, then all double
     quotes will be escaped using ``quot``. By default this is ``&mjf-quot;``.
-    
+
     >>> quote_escape('hello')
     'hello'
     >>> quote_escape('hello\\n')
@@ -234,10 +234,10 @@ def quote_escape(value, lf='&mjf-lf;', quot='&mjf-quot;'):
 def quote_unescape(value, lf='&mjf-lf;', quot='&mjf-quot;'):
     """
     Unescape a string escaped by ``quote_escape``.
-    
-    If it was escaped using anything other than the defaults for ``lf`` and 
+
+    If it was escaped using anything other than the defaults for ``lf`` and
     ``quot`` you must pass them to this function.
-    
+
     >>> quote_unescape("hello&wobble;'&fish;",  '&fish;', '&wobble;')
     'hello"\\'\\n'
     >>> quote_unescape('hello')
@@ -258,14 +258,14 @@ def quote_unescape(value, lf='&mjf-lf;', quot='&mjf-quot;'):
 def simplelist(inline):
     """
     Parse a string to a list.
-        
+
     A simple regex that extracts quoted items from a list.
-    
+
     It retains quotes around elements. (So unquote each element)
-    
+
     >>> simplelist('''hello, goodbye, 'title', "name", "I can't"''')
     ['hello', 'goodbye', "'title'", '"name"', '"I can\\'t"']
-    
+
     FIXME:  This doesn't work fully (allows some badly formed lists):
     e.g.
     >>> simplelist('hello, fish, "wobble" bottom hooray')
@@ -278,27 +278,27 @@ def simplelist(inline):
 # handles lines with comma seperated values on it, followed by a comment
 # correctly handles quoting
 # *and* can handle nested lists - marked between '[...]' or '(...)'
-# See the docstring for how this works 
+# See the docstring for how this works
 # by default it returns a (list, comment) tuple !
 # There are several keyword arguments that control how LineParser works.
 
 class LineParser(object):
     """An object to parse nested lists from strings."""
-    
+
     liststart = { '[' : ']', '(' : ')' }
     quotes = ['\'', '"']
-    
+
     def __init__(self, options=None, **keywargs):
         """Initialise the LineParser."""
         self.reset(options, **keywargs)
-    
+
     def reset(self, options=None, **keywargs):
         """Reset the parser with the specified options."""
         if options is None:
             options = {}
         options.update(keywargs)
         #
-        defaults = { 
+        defaults = {
                     'recursive': True,
                     'comment': True,
                     'retain': False,
@@ -308,8 +308,8 @@ class LineParser(object):
         defaults.update(options)
         if defaults['csv']:
             defaults.update({
-                        'recursive': False, 
-                        'force_list': True, 
+                        'recursive': False,
+                        'force_list': True,
                         'comment': False,
                         })
         # check all the options are valid
@@ -330,27 +330,27 @@ class LineParser(object):
     def feed(self, inline, endchar=None):
         """
         Parse a single line (or fragment).
-        
+
         Uses the options set in the parser object.
-        
-        Can parse lists - including nested lists. (If ``recursive`` is 
+
+        Can parse lists - including nested lists. (If ``recursive`` is
         ``False`` then nested lists will cause a ``BadLineError``).
-        
+
         Return value depends on options.
-        
+
         If ``comment`` is ``False`` it returns ``outvalue``
-        
+
         If ``comment`` is ``True`` it returns ``(outvalue, comment)``. (Even if
         comment is just ``''``).
-        
+
         If ``force_list`` is ``False`` then ``outvalue`` may be a list or a
         single item.
-        
+
         If ``force_list`` is ``True`` then ``outvalue`` will always be a list -
         even if it has just one member.
-        
+
         List syntax :
-        
+
         * Comma separated lines ``a, b, c, d``
         * Lists can optionally be between square or ordinary brackets
             - ``[a, b, c, d]``
@@ -358,19 +358,19 @@ class LineParser(object):
         * Nested lists *must* be between brackets -  ``a, [a, b, c, d], c``
         * A single element list can be shown by a trailing quote - ``a,``
         * An empty list is shown by ``()`` or ``[]``
-        
+
         Elements can be quoted with single or double quotes (but can't contain
         both).
-        
+
         The line can optionally end with a comment (preeded by a '#').
         This depends on the ``comment`` attribute.
-        
+
         If the line is badly built then this method will raise one of : ::
-        
+
             CommentError, BadLineError, UnQuoteError
-        
+
         Using the ``csv`` option is the same as setting : ::
-        
+
                         'recursive': False
                         'force_list': True
                         'comment': False
@@ -421,7 +421,7 @@ class LineParser(object):
             if thischar in self.quotes:
                 # this might raise an error
                 # FIXME: trap the error and raise a more appropriate one ?
-                element, inline = unquote(inline, fullquote=False, 
+                element, inline = unquote(inline, fullquote=False,
                                                     retain=self.retain)
                 inline = inline.lstrip()
                 outlist.append(element)
@@ -467,12 +467,12 @@ class LineParser(object):
 def lineparse(inline, options=None, **keywargs):
     """
     A compatibility function that mimics the old lineparse.
-    
+
     Also more convenient for single line use.
-    
+
     Note: It still uses the new ``LineParser`` - and so takes the same
     keyword arguments as that.
-    
+
     >>> lineparse('''"hello", 'goodbye', "I can't do that", 'You "can" !' # a comment''')
     (['hello', 'goodbye', "I can't do that", 'You "can" !'], '# a comment')
     >>> lineparse('''"hello", 'goodbye', "I can't do that", 'You "can" !' # a comment''', comment=False)
@@ -545,28 +545,28 @@ def lineparse(inline, options=None, **keywargs):
 def list_stringify(inlist):
     """
     Recursively rebuilds a list - making sure all the members are strings.
-    
+
     Can take any iterable or a sequence as the argument and always
     returns a list.
-    
+
     Useful before writing out lists.
-    
+
     Used by makelist if stringify is set.
-    
+
     Uses the ``str`` function for stringification.
-    
+
     Every element will be a string or a unicode object.
-    
+
     Doesn't handle decoding strings into unicode objects (or vice-versa).
-    
+
     >>> list_stringify([2, 2, 2, 2, (3, 3, 2.9)])
     ['2', '2', '2', '2', ['3', '3', '2.9']]
     >>> list_stringify(None)
     Traceback (most recent call last):
-    TypeError: iteration over non-sequence
+    TypeError: 'NoneType' object is not iterable
     >>> list_stringify([])
     []
-    
+
     FIXME: can receive any iterable - e.g. a sequence
     >>> list_stringify('')
     []
@@ -588,24 +588,24 @@ def makelist(inlist, listchar='', stringify=False, escape=False, encoding=None):
     """
     Given a list - turn it into a string that represents that list. (Suitable
     for parsing by ``LineParser``).
-    
+
     listchar should be ``'['``, ``'('`` or ``''``. This is the type of bracket
     used to enclose the list. (``''`` meaning no bracket of course).
-    
+
     If you have nested lists and listchar is ``''``, makelist will
     automatically use ``'['`` for the nested lists.
-    
+
     If stringify is ``True`` (default is ``False``) makelist will stringify the
     inlist first (using ``list_stringify``).
-    
+
     If ``escape`` is ``True`` (default is ``False``) makelist will call
     ``quote_escape`` on each element before passing them to ``elem_quote`` to
     be quoted.
-    
+
     If encoding keyword is not ``None``, all strings are decoded to unicode
     with the specified encoding. Each item will then be a unicode object
     instead of a string.
-    
+
     >>> makelist([])
     '[]'
     >>> makelist(['a', 'b', 'I can\\'t do it', 'Yes you "can" !'])
@@ -636,7 +636,7 @@ def makelist(inlist, listchar='', stringify=False, escape=False, encoding=None):
             outline.append(elem_quote(item, encoding=encoding))
         else:
             # recursive for nested lists
-            outline.append(makelist(item, listchar or '[', 
+            outline.append(makelist(item, listchar or '[',
                                         stringify, escape, encoding))
     return listdict[listchar] % (', '.join(outline))
 
@@ -647,31 +647,31 @@ def makelist(inlist, listchar='', stringify=False, escape=False, encoding=None):
 def csvread(infile):
     """
     Given an infile as an iterable, return the CSV as a list of lists.
-    
+
     infile can be an open file object or a list of lines.
-    
+
     If any of the lines are badly built then a ``CSVError`` will be raised.
     This has a ``csv`` attribute - which is a reference to the parsed CSV.
     Every line that couldn't be parsed will have ``[]`` for it's entry.
-    
+
     The error *also* has an ``errors`` attribute. This is a list of all the
     errors raised. Error in this will have an ``index`` attribute, which is the
     line number, and a ``line`` attribute - which is the actual line that
     caused the error.
-    
+
     Example of usage :
-    
+
     .. raw:: html
-    
+
         {+coloring}
-        
+
         handle = open(filename)
         # remove the trailing '\n' from each line
         the_file = [line.rstrip('\n') for line in handle.readlines()]
         csv = csvread(the_file)
-        
+
         {-coloring}
-    
+
     >>> a = '''"object 1", 'object 2', object 3
     ...     test 1 , "test 2" ,'test 3'
     ...     'obj 1',obj 2,"obj 3"'''
@@ -715,23 +715,23 @@ def csvwrite(inlist, stringify=False):
     """
     Given a list of lists it turns each entry into a line in a CSV.
     (Given a list of lists it returns a list of strings).
-    
+
     The lines will *not* be ``\n`` terminated.
-    
+
     Set stringify to ``True`` (default is ``False``) to convert entries to
     strings before creating the line.
-    
+
     If stringify is ``False`` then any non string value will raise a
     ``TypeError``.
-    
+
     Every member will be quoted using ``elem_quote``, but no escaping is done.
-    
+
     Example of usage :
-    
+
     .. raw:: html
-    
+
         {+coloring}
-        
+
         # escape each entry in each line (optional)
         for index in range(len(the_list)):
             the_list[index] = [quote_escape(val) for val in the_list[index]]
@@ -739,9 +739,9 @@ def csvwrite(inlist, stringify=False):
         the_file = csvwrite(the_list)
         # add a '\n' to each line - ready to write to file
         the_file = [line + '\n' for line in the_file]
-        
+
         {-coloring}
-    
+
     >>> csvwrite([['object 1', 'object 2', 'object 3'], ['test 1', 'test 2', 'test 3'], ['obj 1', 'obj 2', 'obj 3']])
     ['"object 1", "object 2", "object 3"', '"test 1", "test 2", "test 3"', '"obj 1", "obj 2", "obj 3"']
     >>> csvwrite([[3, 3, 3]])
@@ -783,7 +783,7 @@ Triple quote multiline values ?
 
 Doesn't allow Python style string escaping (but has '&mjf-quot;' and '&mjf-lf;').
 
-Uses both \' and \" as quotes and sometimes doesn't quote at all - see 
+Uses both \' and \" as quotes and sometimes doesn't quote at all - see
 elem_quote - may not *always* be compatible with other programs.
 
 Allow space seperated lists ? e.g. 10 5 100 20
@@ -811,8 +811,8 @@ CHANGELOG
 * Removed the ``uni_conv`` option in the CSV functions
 
 .. note::
-    
-    These changes are quite extensive. If any of them cause you problems then 
+
+    These changes are quite extensive. If any of them cause you problems then
     let me know. I can provide a workaround in the next release.
 
 2005/06/01          Version 1.3.0
