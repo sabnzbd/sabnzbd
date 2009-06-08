f619466942afc6b645f3484c0bf95ae5d9ab8759
devrAf <>
2009-06-08 18:01:30 +0000

    [OSX] TO FIX : Growl types translations

--- main/sabnzbd/utils/osx.py
+++ main/sabnzbd/utils/osx.py
@@ -6,17 +6,17 @@
 #
 #You may freely use this code in any way you can think of.
 #"""
-from sabnzbd.lang import T
-NOTIFICATION = {'startup':'grwl-notif-startup','download':'grwl-notif-dl','pp':'grwl-notif-pp','other':'grwl-notif-other'}
+#TO FIX : Translations are not working with this implementation
+#         Growl Registration may only be done once per run ?
+#         Registration is made too early, the language module has not read the text file yet
+#from sabnzbd.lang import T
+#NOTIFICATION = {'startup':'grwl-notif-startup','download':'grwl-notif-dl','pp':'grwl-notif-pp','other':'grwl-notif-other'}
+NOTIFICATION = {'startup':'1. On Startup/Shutdown','download':'2. On adding NZB','pp':'3. On post-processing','other':'4. Other Messages'}
 
 try:
     import Growl
     import os.path
     import logging
-    import logging.handlers
-
-
-    #logging.info('%s/osx/resources/sabnzbdplus.icns' % (sabnzbd.DIR_PROG))
 
     if os.path.isfile('sabnzbdplus.icns'):
         nIcon = Growl.Image.imageFromPath('sabnzbdplus.icns')
@@ -25,14 +25,19 @@ try:
     else:
         nIcon = Growl.Image.imageWithIconForApplication('Terminal')
 
-    def sendGrowlMsg(nTitle , nMsg, nType=T(NOTIFICATION['other'])):
+    def sendGrowlMsg(nTitle , nMsg, nType=NOTIFICATION['other']):
         gnotifier = SABGrowlNotifier(applicationIcon=nIcon)
         gnotifier.register()
+        #TO FIX
+        #gnotifier.notify(T(nType), nTitle, nMsg)
         gnotifier.notify(nType, nTitle, nMsg)
 
     class SABGrowlNotifier(Growl.GrowlNotifier):
-    	applicationName = "SABnzbd"
-    	notifications = T(NOTIFICATION.values())
+        applicationName = "SABnzbd"
+        #TO FIX
+        #notifications = [T(notification) for notification in NOTIFICATION.values()]
+        notifications = NOTIFICATION.values()
+
 except ImportError:
     def sendGrowlMsg(nTitle , nMsg):
         pass
