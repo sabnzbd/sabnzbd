cd5e4779a5dbb7ac1c94fcf3f6236759cc799466
shypike <>
2009-11-06 21:07:31 +0000

    Add standard copyright header.

--- main/sabnzbd/utils/osx.py
+++ main/sabnzbd/utils/osx.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python -OO
-#"""OSX Growl Notification
+# Copyright 2008-2009 The SABnzbd-Team <team@sabnzbd.org>
 #
-#This library expose Growl notifications
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#You may freely use this code in any way you can think of.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
 #"""
 #TO FIX : Translations are not working with this implementation
 #         Growl Registration may only be done once per run ?
