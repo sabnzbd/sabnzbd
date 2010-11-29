0bd763833f83ad74376a656e4021d973a0185496
ShyPike <>
2010-11-29 22:56:47 +0100

    Fix incorrect URL used for browser launch (side-effect of previous change).

--- SABnzbd.py
+++ SABnzbd.py
@@ -647,7 +647,7 @@ def check_for_sabnzbd(url, upload_nzbs):
                 upload_file(url, f)
         else:
             # Launch the web browser and quit since sabnzbd is already running
-            launch_a_browser(url, force=True)
+            launch_a_browser(url.replace('api?', ''), force=True)
         exit_sab(0)
         return True
     return False
