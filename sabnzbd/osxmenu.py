1e10308d39c2a8f4d3b0f9c4e7247660c3743d1b
ShyPike <shypike@sabnzbd.org>
2012-02-01 22:34:04 +0100

    OSX troubleshoot menu: when using 127.0.0.1 also reset http and https ports.

--- sabnzbd/osxmenu.py
+++ sabnzbd/osxmenu.py
@@ -282,7 +282,7 @@ class SABnzbdDelegate(NSObject):
 
         self.diagnostic_menu_item.setSubmenu_(self.menu_diagnostic)
         self.menu.addItem_(self.diagnostic_menu_item)
-        
+
         if (debug == 1) : NSLog("[osx] menu 16 Diagnostic added")
 
         #Quit Item
@@ -744,6 +744,8 @@ class SABnzbdDelegate(NSObject):
 
     def restartSafeHost_(self, sender):
         sabnzbd.cfg.cherryhost.set('127.0.0.1')
+        sabnzbd.cfg.cherryport.set('8080')
+        sabnzbd.cfg.https_port.set('8090')
         sabnzbd.cfg.enable_https.set(False)
         sabnzbd.config.save_config()
         self.setMenuTitle("\n\n%s\n"% (T('Stopping...')))
