41bc05a10821efbcc2068d2df176e017ea03a91e
ShyPike <shypike@sabnzbd.org>
2012-02-21 21:32:24 +0100

    Enable "uniconfig" option by default.

--- sabnzbd/cfg.py
+++ sabnzbd/cfg.py
@@ -232,7 +232,7 @@ quota_period = OptionStr('misc', 'quota_period', 'm')
 osx_menu = OptionBool('misc', 'osx_menu', True)
 osx_speed = OptionBool('misc', 'osx_speed', True)
 win_menu = OptionBool('misc', 'win_menu', True)
-uniconfig = OptionBool('misc', 'uniconfig', False)
+uniconfig = OptionBool('misc', 'uniconfig', True)
 
 #------------------------------------------------------------------------------
 # Set root folders for Folder config-items
