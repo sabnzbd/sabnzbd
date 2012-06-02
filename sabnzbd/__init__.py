5337ade3fb6791ec2a0b11d330108d0e0e98441b
ShyPike <shypike@sabnzbd.org>
2012-06-02 15:57:08 +0200

    Set article cache to 200M when not already set.

--- sabnzbd/__init__.py
+++ sabnzbd/__init__.py
@@ -250,6 +250,8 @@ def initialize(pause_downloader = False, clean_up = False, evalSched=False, repa
     sabnzbd.encoding.change_fsys(cfg.fsys_type())
 
     ### Set cache limit
+    if (sabnzbd.WIN32 or sabnzbd.DARWIN) and not cfg.cache_limit():
+        cfg.cache_limit.set('200M')
     ArticleCache.do.new_limit(cfg.cache_limit.get_int())
 
     check_incomplete_vs_complete()
