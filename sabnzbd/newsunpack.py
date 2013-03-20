635ba9d98b7ded50340570f567fd1dc1f4f24075
shypike <shypike@sabnzbd.org>
2013-03-20 21:54:47 +0100

    Don't try to join a set of just one file (e.g. IMAGE.000) and reduce memory usage when joining large segments.
    When there a single file called something like IMAGE.000, don't try to join it.
    The joining procedure tries to read an entire segment file into memory, this may lead to a string overflow.
    Use shutil.copyfileobj() with a 24 MB buffer instead.

--- sabnzbd/newsunpack.py
+++ sabnzbd/newsunpack.py
@@ -26,6 +26,7 @@ import subprocess
 import logging
 from time import time
 import binascii
+import shutil
 
 import sabnzbd
 from sabnzbd.encoding import TRANS, UNTRANS, unicode2local, name_fixer, \
@@ -306,7 +307,6 @@ def get_seq_number(name):
         match, set, num = match_ts(name)
     else:
         num = tail[1:]
-    assert isinstance(num, str)
     if num.isdigit():
         return int(num)
     else:
@@ -317,6 +317,7 @@ def file_join(nzo, workdir, workdir_complete, delete, joinables):
         when succesful, delete originals
     """
     newfiles = []
+    bufsize = 24*1024*1024
 
     # Create matching sets from the list of files
     joinable_sets = {}
@@ -347,6 +348,11 @@ def file_join(nzo, workdir, workdir_complete, delete, joinables):
                 # done, go to next set
                 continue
 
+            # Only join when there is more than one file
+            size = len(current)
+            if size < 2:
+                continue
+
             # Prepare joined file
             filename = joinable_set
             if workdir_complete:
@@ -355,7 +361,6 @@ def file_join(nzo, workdir, workdir_complete, delete, joinables):
             joined_file = open(filename, 'ab')
 
             # Join the segments
-            size = len(current)
             n = get_seq_number(current[0])
             seq_error = n > 1
             for joinable in current:
@@ -365,7 +370,7 @@ def file_join(nzo, workdir, workdir_complete, delete, joinables):
                 logging.debug("Processing %s", joinable)
                 nzo.set_action_line(T('Joining'), '%.0f%%' % perc)
                 f = open(joinable, 'rb')
-                joined_file.write(f.read())
+                shutil.copyfileobj(f, joined_file, bufsize)
                 f.close()
                 if delete:
                     logging.debug("Deleting %s", joinable)
