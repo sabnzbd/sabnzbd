sabnzbd/sabnzbd

commit 5f1c9cc21ab73c7e231e5d31971df44c7f8af8d9

Log unpack passwords that are read from a file.

sabnzbd/newsunpack.py
@@ -507,7 +507,7 @@ def rar_extract(rarfile, numrars, one_folder, nzo, setname, extraction_path):
     new_files = None
     rars = []
     if nzo.password:
-        logging.info('Got a password set by user')
+        logging.info('Got a password set by user: %s', password)
         passwords = [nzo.password.strip()]
     else:
         passwords = []
@@ -522,6 +522,7 @@ def rar_extract(rarfile, numrars, one_folder, nzo, setname, extraction_path):
                 lines = pwf.read().split('\n')
                 # Remove empty lines and space-only passwords and remove surrounding spaces
                 pws = [pw.strip('\r\n ') for pw in lines if pw.strip('\r\n ')]
+                logging.debug('Read these passwords from file: %s', pws)
                 passwords.extend(pws)
                 pwf.close()
                 logging.info('Read %s passwords from file %s', len(pws), pw_file)

commit 6cc9e8e66181f7bd08a3acac2fc11d70292a8461

Fix crash error in logging
Fix problem introduced in commit 5f1c9cc21ab73c7e231e5d31971df44c7f8af8d9

sabnzbd/newsunpack.py
@@ -507,7 +507,7 @@ def rar_extract(rarfile, numrars, one_folder, nzo, setname, extraction_path):
     new_files = None
     rars = []
     if nzo.password:
-        logging.info('Got a password set by user: %s', password)
+        logging.info('Got a password set by user: %s', nzo.password)
         passwords = [nzo.password.strip()]
     else:
         passwords = []