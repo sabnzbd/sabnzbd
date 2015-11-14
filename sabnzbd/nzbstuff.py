sabnzbd/sabnzbd

commit 8a166729d4583926bd513641e3d97aabfe9ee6f7

Add option to enable duplicate checking from backup folder.

sabnzbd/nzbstuff.py
@@ -1552,6 +1552,9 @@ class NzbObject(TryList):
         if no_dupes:
             res = history_db.have_md5sum(self.md5sum)
             logging.debug('Dupe checking NZB in history: filename=%s, md5sum=%s, result=%s', self.filename, self.md5sum, res)
+            if not res and cfg.backup_for_duplicates():
+                res = sabnzbd.backup_exists(self.filename)
+                logging.debug('Dupe checking NZB against backup: filename=%s, result=%s', self.filename, res)
         # dupe check off nzb filename
         if not res and no_series_dupes:
             series, season, episode, dummy = sabnzbd.newsunpack.analyse_show(self.final_name)

sabnzbd/__init__.py
@@ -557,8 +557,7 @@ NZB_LOCK = Lock()
 def backup_exists(filename):
     """ Return True if backup exists and no_dupes is set """
     path = cfg.nzb_backup_dir.get_path()
-    return path and sabnzbd.cfg.no_dupes() and \
-           os.path.exists(os.path.join(path, filename + '.gz'))
+    return path and os.path.exists(os.path.join(path, filename + '.gz'))
 
 
 def backup_nzb(filename, data):

sabnzbd/cfg.py
@@ -140,6 +140,7 @@ replace_spaces = OptionBool('misc', 'replace_spaces', False)
 replace_dots = OptionBool('misc', 'replace_dots', False)
 no_dupes = OptionNumber('misc', 'no_dupes', 0)
 no_series_dupes = OptionNumber('misc', 'no_series_dupes', 0)
+backup_for_duplicates = OptionBool('misc', 'backup_for_duplicates', True)
 
 ignore_samples = OptionNumber('misc', 'ignore_samples', 0, 0, 2)
 create_group_folders = OptionBool('misc', 'create_group_folders', False)

sabnzbd/interface.py
@@ -1364,7 +1364,7 @@ SPECIAL_BOOL_LIST = \
               'osx_menu', 'osx_speed', 'win_menu', 'use_pickle', 'allow_incomplete_nzb',
               'no_ipv6', 'keep_awake', 'empty_postproc',
               'web_watchdog', 'wait_for_dfolder', 'warn_empty_nzb', 'enable_bonjour',
-              'warn_dupl_jobs', 'enable_par_cleanup',
+              'warn_dupl_jobs', 'backup_for_duplicates', 'enable_par_cleanup',
               'enable_https_verification', 'api_logging'
      )
 SPECIAL_VALUE_LIST = \
@@ -2427,7 +2427,7 @@ class Status(object):
 
         header['folders'] = [xml_name(item) for item in sabnzbd.nzbqueue.scan_jobs(all=False, action=False)]
         header['configfn'] = xml_name(config.get_filename())
-    
+
         # Dashboard: Begin
         if not kwargs.get('skip_dashboard'):
             from sabnzbd.utils.getipaddress import localipv4, publicipv4, ipv6
@@ -2441,7 +2441,7 @@ class Status(object):
                 header['dnslookup'] = "OK"
             except:
                 header['dnslookup'] = None
-    
+
             # Dashboard: Speed of System
             from sabnzbd.utils.getperformance import getpystone, getcpu
             header['pystone'] = getpystone()
@@ -2462,7 +2462,7 @@ class Status(object):
                 # does not yet exist, so create it:
                 sabnzbd.completedirspeed = -1  # -1 means ... not yet determined
             header['completedirspeed'] = sabnzbd.completedirspeed
-    
+
             try:
                 sabnzbd.dashrefreshcounter  # The persistent var @UndefinedVariable
             except:

commit 1c388faf42767c99a51c2337823cd5395ccc17a7

Fix option to enable duplicate checking from backup folder.
Fixed error introduced by 8a166729d4583926bd513641e3d97aabfe9ee6f7
Closes issue #390

sabnzbd/nzbstuff.py
@@ -675,6 +675,12 @@ class NzbObject(TryList):
         wdir = long_path(os.path.join(cfg.download_dir.get_path(), self.work_name))
         adir = os.path.join(wdir, JOB_ADMIN)
 
+        # Check against identical checksum or series/season/episode
+        if (not reuse) and nzb and dup_check and priority != REPAIR_PRIORITY:
+            duplicate, series = self.has_duplicates()
+        else:
+            duplicate = series = 0
+
         if reuse:
             remove_all(adir, 'SABnzbd_nz?_*')
             remove_all(adir, 'SABnzbd_article_*')
@@ -726,12 +732,6 @@ class NzbObject(TryList):
 
             sabnzbd.backup_nzb(filename, nzb)
             sabnzbd.save_compressed(adir, filename, nzb)
-
-        # Check against identical checksum or series/season/episode
-        if (not reuse) and nzb and dup_check and priority != REPAIR_PRIORITY:
-            duplicate, series = self.has_duplicates()
-        else:
-            duplicate = series = 0
 
         if not self.files and not reuse:
             self.purge_data(keep_basic=False)