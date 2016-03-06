e2f51595b686e9ace771254fcceb1787688d0a38
Sander Jonkers <sander.jonkers@gmail.com>
2016-03-06 01:39:41 +0100

    Because of dual IPv4/IPv6 clients, finding the public ipv4 needs special attention

--- sabnzbd/getipaddress.py
+++ sabnzbd/getipaddress.py
@@ -36,13 +36,39 @@ def localipv4():
     return ipv4
 
 def publicipv4():
+    # Because of dual IPv4/IPv6 clients, finding the public ipv4 needs special attention, 
+    # meaning forcing IPv4 connections, and not allowing IPv6 connections
     try:
         import urllib2
-        req = urllib2.Request("http://" + sabnzbd.cfg.selftest_host(), headers={'User-Agent': 'SABnzbd+/%s' % sabnzbd.version.__version__}) 
-        f = urllib2.urlopen(req, timeout=2)    # timeout 2 seconds, in case website is not accessible
-        public_ipv4 = f.read()
-        socket.inet_aton(public_ipv4)  # if we got anything else than a plain IPv4 address, this will raise an exception
+        ipv4_found = False
+        # we only want IPv4 resolving, so socket.AF_INET:
+        result = socket.getaddrinfo(sabnzbd.cfg.selftest_host(), 80, socket.AF_INET, 0, socket.IPPROTO_TCP)
     except:
+	# something very bad: no urllib2, no resolving of selftest_host, no network at all
+        public_ipv4 = None
+        return public_ipv4
+    # we got one or more IPv4 address(es), so let's connect to them
+    for item in result:
+        selftest_ipv4 = item[4][0]     # get next IPv4 address of sabnzbd.cfg.selftest_host()
+        try:
+            # put the selftest_host's IPv4 address into the URL
+            req = urllib2.Request("http://" + selftest_ipv4 + "/")
+            # specify the User-Agent, because certain sites refuse connections with "python urllib2" as User-Agent:
+            req.add_header('User-Agent', 'SABnzbd+/%s' % sabnzbd.version.__version__ )
+            # specify the Host, because we only provide the IPv4 address in the URL: 
+            req.add_header('Host', sabnzbd.cfg.selftest_host())
+            # get the response
+            public_ipv4 = urllib2.urlopen(req, timeout=2).read() # timeout 2 seconds, in case website is not accessible
+            # ... check the response is indeed an IPv4 address:
+            socket.inet_aton(public_ipv4)  # if we got anything else than a plain IPv4 address, this will raise an exception
+            # if we get here without exception, we're done:
+            ipv4_found = True
+            break
+        except:
+            # the connect OR the inet_aton raised an exception, so:
+            # continue the for loop to try next server IPv4 address
+            pass
+    if not ipv4_found :
         public_ipv4 = None
     return public_ipv4
 
