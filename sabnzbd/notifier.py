sabnzbd/sabnzbd

commit 91f5eea7ba64b513016ca3402b3cf1fdfabd063a

Notifier: Use get_prio function to get priorities

sabnzbd/notifier.py
@@ -17,7 +17,7 @@
 #
 
 """
-sabnzbd.growler - Send notifications to Growl
+sabnzbd.notifier - Send notifications to any notification services
 """
 
 from __future__ import with_statement
@@ -116,6 +116,14 @@ def check_classes(gtype, section):
     except TypeError:
         logging.debug('Incorrect Notify option %s:%s_prio_%s', section, section, gtype)
 
+def get_prio(gtype, section):
+    """ Check if `gtype` is enabled in `section` """
+    try:
+        return sabnzbd.config.get_config(section, '%s_prio_%s' % (section, gtype))() > 0
+    except TypeError:
+        logging.debug('Incorrect Notify option %s:%s_prio_%s', section, section, gtype)
+        return None
+
 
 def send_notification(title, msg, gtype):
     """ Send Notification message """
@@ -151,7 +159,7 @@ def send_notification(title, msg, gtype):
 
     # Pushbullet
     if sabnzbd.cfg.pushbullet_enable():
-        if sabnzbd.cfg.pushbullet_apikey():
+        if sabnzbd.cfg.pushbullet_apikey() and check_classes(gtype, 'pushbullet'):
             Thread(target=send_pushbullet, args=(title, msg, gtype)).start()
             time.sleep(0.5)
 
@@ -377,28 +385,8 @@ def send_prowl(title, msg, gtype, force=False, test=None):
     title = Tx(NOTIFICATION.get(gtype, 'other'))
     title = urllib2.quote(title.encode('utf8'))
     msg = urllib2.quote(msg.encode('utf8'))
-    prio = -3
+    prio = get_prio(gtype, 'prowl');
 
-    if gtype == 'startup':
-        prio = sabnzbd.cfg.prowl_prio_startup()
-    if gtype == 'download':
-        prio = sabnzbd.cfg.prowl_prio_download()
-    if gtype == 'pp':
-        prio = sabnzbd.cfg.prowl_prio_pp()
-    if gtype == 'complete':
-        prio = sabnzbd.cfg.prowl_prio_complete()
-    if gtype == 'failed':
-        prio = sabnzbd.cfg.prowl_prio_failed()
-    if gtype == 'disk_full':
-        prio = sabnzbd.cfg.prowl_prio_disk_full()
-    if gtype == 'warning':
-        prio = sabnzbd.cfg.prowl_prio_warning()
-    if gtype == 'error':
-        prio = sabnzbd.cfg.prowl_prio_error()
-    if gtype == 'queue_done':
-        prio = sabnzbd.cfg.prowl_prio_queue_done()
-    if gtype == 'other':
-        prio = sabnzbd.cfg.prowl_prio_other()
     if force:
         prio = 0
 
@@ -430,28 +418,8 @@ def send_pushover(title, msg, gtype, force=False, test=None):
         return T('Cannot send, missing required data')
 
     title = Tx(NOTIFICATION.get(gtype, 'other'))
-    prio = -2
+    prio = get_prio(gtype, 'pushover');
 
-    if gtype == 'startup':
-        prio = sabnzbd.cfg.pushover_prio_startup()
-    if gtype == 'download':
-        prio = sabnzbd.cfg.pushover_prio_download()
-    if gtype == 'pp':
-        prio = sabnzbd.cfg.pushover_prio_pp()
-    if gtype == 'complete':
-        prio = sabnzbd.cfg.pushover_prio_complete()
-    if gtype == 'failed':
-        prio = sabnzbd.cfg.pushover_prio_failed()
-    if gtype == 'disk_full':
-        prio = sabnzbd.cfg.pushover_prio_disk_full()
-    if gtype == 'warning':
-        prio = sabnzbd.cfg.pushover_prio_warning()
-    if gtype == 'error':
-        prio = sabnzbd.cfg.pushover_prio_error()
-    if gtype == 'queue_done':
-        prio = sabnzbd.cfg.pushover_prio_queue_done()
-    if gtype == 'other':
-        prio = sabnzbd.cfg.pushover_prio_other()
     if force:
         prio = 1
 
@@ -490,28 +458,27 @@ def send_pushbullet(title, msg, gtype, force=False, test=None):
         return T('Cannot send, missing required data')
 
     title = u'SABnzbd: ' + Tx(NOTIFICATION.get(gtype, 'other'))
+    
+    try:
+        conn = httplib.HTTPSConnection('api.pushbullet.com:443')
+        conn.request('POST', '/v2/pushes',
+            json.dumps({
+                'type': 'note',
+                'device': device,
+                'title': title,
+                'body': msg}),
+            headers={'Authorization': 'Bearer ' + apikey,
+                     'Content-type': 'application/json'})
+        res = conn.getresponse()
+        if res.status != 200:
+            logging.error(T('Bad response from Pushbullet (%s): %s'), res.status, res.read())
+        else:
+            logging.info('Successfully sent to Pushbullet')
 
-    if force or check_classes(gtype, 'nscript'):
-        try:
-            conn = httplib.HTTPSConnection('api.pushbullet.com:443')
-            conn.request('POST', '/v2/pushes',
-                json.dumps({
-                    'type': 'note',
-                    'device': device,
-                    'title': title,
-                    'body': msg}),
-                headers={'Authorization': 'Bearer ' + apikey,
-                         'Content-type': 'application/json'})
-            res = conn.getresponse()
-            if res.status != 200:
-                logging.error(T('Bad response from Pushbullet (%s): %s'), res.status, res.read())
-            else:
-                logging.info('Successfully sent to Pushbullet')
-
-        except:
-            logging.warning(T('Failed to send pushbullet message'))
-            logging.info('Traceback: ', exc_info=True)
-            return T('Failed to send pushbullet message')
+    except:
+        logging.warning(T('Failed to send pushbullet message'))
+        logging.info('Traceback: ', exc_info=True)
+        return T('Failed to send pushbullet message')
     return ''
 
 def send_windows(title, msg, gtype):

sabnzbd/__init__.py
@@ -260,13 +260,13 @@ def initialize(pause_downloader=False, clean_up=False, evalSched=False, repair=0
     cfg.enable_https.callback(guard_restart)
     cfg.top_only.callback(guard_top_only)
     cfg.pause_on_post_processing.callback(guard_pause_on_pp)
-    cfg.growl_server.callback(sabnzbd.growler.change_value)
-    cfg.growl_password.callback(sabnzbd.growler.change_value)
+    cfg.growl_server.callback(sabnzbd.notifier.change_value)
+    cfg.growl_password.callback(sabnzbd.notifier.change_value)
     cfg.quota_size.callback(guard_quota_size)
     cfg.quota_day.callback(guard_quota_dp)
     cfg.quota_period.callback(guard_quota_dp)
     cfg.fsys_type.callback(guard_fsys_type)
-    cfg.language.callback(sabnzbd.growler.reset_growl)
+    cfg.language.callback(sabnzbd.notifier.reset_growl)
     cfg.enable_https_verification.callback(guard_https_ver)
     guard_https_ver()
 

sabnzbd/api.py
@@ -64,7 +64,7 @@ from sabnzbd.bpsmeter import BPSMeter
 from sabnzbd.rating import Rating
 from sabnzbd.getipaddress import localipv4, publicipv4, ipv6
 from sabnzbd.database import build_history_info, unpack_history_info, get_history_handle
-import sabnzbd.growler
+import sabnzbd.notifier
 import sabnzbd.rss
 import sabnzbd.emailer
 
@@ -725,48 +725,48 @@ def _api_test_email(name, output, kwargs):
 def _api_test_windows(name, output, kwargs):
     """ API: send a test to Windows, return result """
     logging.info("Sending test notification")
-    res = sabnzbd.growler.send_windows('SABnzbd', T('Test Notification'), 'other')
+    res = sabnzbd.notifier.send_windows('SABnzbd', T('Test Notification'), 'other')
     return report(output, error=res)
 
 def _api_test_notif(name, output, kwargs):
     """ API: send a test to Notification Center, return result """
     logging.info("Sending test notification")
-    res = sabnzbd.growler.send_notification_center('SABnzbd', T('Test Notification'), 'other')
+    res = sabnzbd.notifier.send_notification_center('SABnzbd', T('Test Notification'), 'other')
     return report(output, error=res)
 
 
 def _api_test_growl(name, output, kwargs):
     """ API: send a test Growl notification, return result """
     logging.info("Sending Growl notification")
-    res = sabnzbd.growler.send_growl('SABnzbd', T('Test Notification'), 'other', test=kwargs)
+    res = sabnzbd.notifier.send_growl('SABnzbd', T('Test Notification'), 'other', test=kwargs)
     return report(output, error=res)
 
 
 def _api_test_osd(name, output, kwargs):
     """ API: send a test OSD notification, return result """
     logging.info("Sending OSD notification")
-    res = sabnzbd.growler.send_notify_osd('SABnzbd', T('Test Notification'))
+    res = sabnzbd.notifier.send_notify_osd('SABnzbd', T('Test Notification'))
     return report(output, error=res)
 
 
 def _api_test_prowl(name, output, kwargs):
     """ API: send a test Prowl notification, return result """
     logging.info("Sending Prowl notification")
-    res = sabnzbd.growler.send_prowl('SABnzbd', T('Test Notification'), 'other', force=True, test=kwargs)
+    res = sabnzbd.notifier.send_prowl('SABnzbd', T('Test Notification'), 'other', force=True, test=kwargs)
     return report(output, error=res)
 
 
 def _api_test_pushover(name, output, kwargs):
     """ API: send a test Pushover notification, return result """
     logging.info("Sending Pushover notification")
-    res = sabnzbd.growler.send_pushover('SABnzbd', T('Test Notification'), 'other', force=True, test=kwargs)
+    res = sabnzbd.notifier.send_pushover('SABnzbd', T('Test Notification'), 'other', force=True, test=kwargs)
     return report(output, error=res)
 
 
 def _api_test_pushbullet(name, output, kwargs):
     """ API: send a test Pushbullet notification, return result """
     logging.info("Sending Pushbullet notification")
-    res = sabnzbd.growler.send_pushbullet('SABnzbd', T('Test Notification'), 'other', force=True, test=kwargs)
+    res = sabnzbd.notifier.send_pushbullet('SABnzbd', T('Test Notification'), 'other', force=True, test=kwargs)
     return report(output, error=res)
 
 

sabnzbd/downloader.py
@@ -33,7 +33,7 @@ from sabnzbd.decorators import synchronized, synchronized_CV, CV
 from sabnzbd.decoder import Decoder
 from sabnzbd.utils.sslinfo import ssl_protocols
 from sabnzbd.newswrapper import NewsWrapper, request_server_info
-import sabnzbd.growler as growler
+import sabnzbd.notifier as notifier
 from sabnzbd.constants import *
 import sabnzbd.config as config
 import sabnzbd.cfg as cfg
@@ -240,7 +240,7 @@ class Downloader(Thread):
     def resume(self):
         logging.info("Resuming")
         if self.paused:
-            growler.send_notification("SABnzbd", T('Resuming'), 'download')
+            notifier.send_notification("SABnzbd", T('Resuming'), 'download')
         self.paused = False
 
     @synchronized_CV
@@ -249,7 +249,7 @@ class Downloader(Thread):
         if not self.paused:
             self.paused = True
             logging.info("Pausing")
-            growler.send_notification("SABnzbd", T('Paused'), 'download')
+            notifier.send_notification("SABnzbd", T('Paused'), 'download')
             if self.is_paused():
                 BPSMeter.do.reset()
             if cfg.autodisconnect():
@@ -852,7 +852,7 @@ class Downloader(Thread):
 
     def stop(self):
         self.shutdown = True
-        growler.send_notification("SABnzbd", T('Shutting down'), 'startup')
+        notifier.send_notification("SABnzbd", T('Shutting down'), 'startup')
 
 
 def stop():

sabnzbd/interface.py
@@ -2823,8 +2823,8 @@ class ConfigNotify(object):
         conf['my_home'] = sabnzbd.DIR_HOME
         conf['lastmail'] = self.__lastmail
         conf['have_growl'] = True
-        conf['have_ntfosd'] = sabnzbd.growler.have_ntfosd()
-        conf['have_ncenter'] = sabnzbd.DARWIN_VERSION > 7 and bool(sabnzbd.growler.ncenter_path())
+        conf['have_ntfosd'] = sabnzbd.notifier.have_ntfosd()
+        conf['have_ncenter'] = sabnzbd.DARWIN_VERSION > 7 and bool(sabnzbd.notifier.ncenter_path())
 
         for kw in LIST_EMAIL:
             conf[kw] = config.get_config('misc', kw).get_string()
@@ -2845,7 +2845,7 @@ class ConfigNotify(object):
             conf[kw] = config.get_config('acenter', kw)()
         for kw in LIST_NTFOSD:
             conf[kw] = config.get_config('ntfosd', kw)()
-        conf['notify_texts'] = sabnzbd.growler.NOTIFICATION
+        conf['notify_texts'] = sabnzbd.notifier.NOTIFICATION
 
         template = Template(file=os.path.join(self.__web_dir, 'config_notify.tmpl'),
                             filter=FILTER, searchList=[conf], compilerSettings=DIRECTIVES)

sabnzbd/nzbqueue.py
@@ -42,7 +42,7 @@ import sabnzbd.cfg as cfg
 from sabnzbd.articlecache import ArticleCache
 import sabnzbd.downloader
 from sabnzbd.assembler import Assembler, file_has_articles
-import sabnzbd.growler as growler
+import sabnzbd.notifier as notifier
 from sabnzbd.encoding import platform_encode
 from sabnzbd.bpsmeter import BPSMeter
 
@@ -426,7 +426,7 @@ class NzbQueue(TryList):
                 self.save(nzo)
 
             if not (quiet or nzo.status in ('Fetching',)):
-                growler.send_notification(T('NZB added to queue'), nzo.filename, 'download')
+                notifier.send_notification(T('NZB added to queue'), nzo.filename, 'download')
 
         if cfg.auto_sort():
             self.sort_by_avg_age()

sabnzbd/osxmenu.py
@@ -42,7 +42,7 @@ import sabnzbd.cfg
 from sabnzbd.constants import *
 from sabnzbd.misc import get_filename, get_ext, diskfree, to_units
 from sabnzbd.panic import launch_a_browser
-import sabnzbd.growler as growler
+import sabnzbd.notifier as notifier
 
 from sabnzbd.api import fast_queue
 from sabnzbd.nzbqueue import NzbQueue
@@ -546,7 +546,7 @@ class SABnzbdDelegate(NSObject):
             if sabnzbd.NEW_VERSION and self.version_notify:
                 # logging.info("[osx] New Version : %s" % (sabnzbd.NEW_VERSION))
                 new_release, _new_rel_url = sabnzbd.NEW_VERSION
-                growler.send_notification("SABnzbd", "%s : %s" % (T('New release available'), new_release), 'other')
+                notifier.send_notification("SABnzbd", "%s : %s" % (T('New release available'), new_release), 'other')
                 self.version_notify = 0
         except:
             logging.info("[osx] versionUpdate Exception %s" % (sys.exc_info()[0]))
@@ -780,7 +780,7 @@ class SABnzbdDelegate(NSObject):
         cherrypy.engine.exit()
         sabnzbd.SABSTOP = True
         try:
-            growler.send_notification('SABnzbd', T('SABnzbd shutdown finished'), growler.NOTIFICATION['other'])
+            notifier.send_notification('SABnzbd', T('SABnzbd shutdown finished'), notifier.NOTIFICATION['other'])
         except AttributeError:
             # Fails for the OSX binary
             pass

sabnzbd/postproc.py
@@ -46,7 +46,7 @@ import sabnzbd.config as config
 import sabnzbd.cfg as cfg
 import sabnzbd.nzbqueue
 import sabnzbd.database as database
-import sabnzbd.growler as growler
+import sabnzbd.notifier as notifier
 
 
 class PostProcessor(Thread):
@@ -508,10 +508,10 @@ def process_job(nzo):
 
         # Show final status in history
         if all_ok:
-            growler.send_notification(T('Download Completed'), filename, 'complete')
+            notifier.send_notification(T('Download Completed'), filename, 'complete')
             nzo.status = Status.COMPLETED
         else:
-            growler.send_notification(T('Download Failed'), filename, 'failed')
+            notifier.send_notification(T('Download Failed'), filename, 'failed')
             nzo.status = Status.FAILED
 
     except:
@@ -520,7 +520,7 @@ def process_job(nzo):
             logging.info("Traceback: ", exc_info=True)
             crash_msg = T('see logfile')
         nzo.fail_msg = T('PostProcessing was aborted (%s)') % unicoder(crash_msg)
-        growler.send_notification(T('Download Failed'), filename, 'failed')
+        notifier.send_notification(T('Download Failed'), filename, 'failed')
         nzo.status = Status.FAILED
         par_error = True
         all_ok = False
@@ -574,7 +574,7 @@ def parring(nzo, workdir):
     """ Perform par processing. Returns: (par_error, re_add) """
     assert isinstance(nzo, sabnzbd.nzbstuff.NzbObject)
     filename = nzo.final_name
-    growler.send_notification(T('Post-processing'), nzo.final_name, 'pp')
+    notifier.send_notification(T('Post-processing'), nzo.final_name, 'pp')
     logging.info('Par2 check starting on %s', filename)
 
     # Get verification status of sets

commit 3435886fe7debf9cda4d2be6858913f243c49b0a

Fix priority issue in PushOver and PushBullet.
Fixes error introduced in 91f5eea7ba64b513016ca3402b3cf1fdfabd063a

sabnzbd/notifier.py
@@ -117,14 +117,15 @@ def check_classes(gtype, section):
         return sabnzbd.config.get_config(section, '%s_prio_%s' % (section, gtype))() > 0
     except TypeError:
         logging.debug('Incorrect Notify option %s:%s_prio_%s', section, section, gtype)
+        return False
 
 def get_prio(gtype, section):
     """ Check if `gtype` is enabled in `section` """
     try:
-        return sabnzbd.config.get_config(section, '%s_prio_%s' % (section, gtype))() > 0
+        return sabnzbd.config.get_config(section, '%s_prio_%s' % (section, gtype))()
     except TypeError:
         logging.debug('Incorrect Notify option %s:%s_prio_%s', section, section, gtype)
-        return None
+        return -1000
 
 
 def send_notification(title, msg, gtype):
@@ -466,7 +467,7 @@ def send_pushbullet(title, msg, gtype, force=False, test=None):
         return T('Cannot send, missing required data')
 
     title = u'SABnzbd: ' + Tx(NOTIFICATION.get(gtype, 'other'))
-    
+
     try:
         conn = httplib.HTTPSConnection('api.pushbullet.com:443')
         conn.request('POST', '/v2/pushes',