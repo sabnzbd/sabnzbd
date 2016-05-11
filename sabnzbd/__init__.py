d184d99026a5f4780e3fc055e8f14aae3ff6da59
shypike <shypike@sabnzbd.org>
2016-05-11 19:07:20 +0200

    Set default cache size to 450MB on Windows and OSX.

--- sabnzbd/__init__.py
+++ sabnzbd/__init__.py
@@ -277,8 +277,9 @@ def initialize(pause_downloader=False, clean_up=False, evalSched=False, repair=0
     sabnzbd.encoding.change_fsys(cfg.fsys_type())
 
     # Set cache limit
-    if (sabnzbd.WIN32 or sabnzbd.DARWIN) and not cfg.cache_limit():
-        cfg.cache_limit.set('200M')
+    if sabnzbd.WIN32 or sabnzbd.DARWIN:
+        if cfg.cache_limit() == '' or cfg.cache_limit() == '200M':
+            cfg.cache_limit.set('450M')
     ArticleCache.do.new_limit(cfg.cache_limit.get_int())
 
     check_incomplete_vs_complete()
