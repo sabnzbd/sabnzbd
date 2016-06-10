6682ba37d4dfbfdeac0312ab0381060b5f4dbf52
shypike <shypike@sabnzbd.org>
2016-06-10 18:41:27 +0200

    Prevent post-processor crash when end-of-queue script does not exist.

--- sabnzbd/__init__.py
+++ sabnzbd/__init__.py
@@ -798,11 +798,12 @@ def change_queue_complete_action(action, new=True):
 def run_script(script):
     """ Run a user script (queue complete only) """
     command = [os.path.join(cfg.script_dir.get_path(), script)]
-    stup, need_shell, command, creationflags = sabnzbd.newsunpack.build_command(command)
-    logging.info('Spawning external command %s', command)
-    subprocess.Popen(command, shell=need_shell, stdin=subprocess.PIPE,
-                     stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                     startupinfo=stup, creationflags=creationflags)
+    if os.path.exists(command[0]):
+        stup, need_shell, command, creationflags = sabnzbd.newsunpack.build_command(command)
+        logging.info('Spawning external command %s', command)
+        subprocess.Popen(command, shell=need_shell, stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                         startupinfo=stup, creationflags=creationflags)
 
 
 def empty_queues():
