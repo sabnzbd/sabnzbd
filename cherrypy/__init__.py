811b625d522c533b9af6cee6d820b6149eb4b82b
shypike <shypike@sabnzbd.org>
2016-07-01 22:03:13 +0200

    CherryPy 6.0.2 patch: hard-code the version number

--- cherrypy/__init__.py
+++ cherrypy/__init__.py
@@ -92,7 +92,7 @@ try:
     __version__ = pkg_resources.require('cherrypy')[0].version
 except Exception:
     __version__ = 'unknown'
-
+__version__ = '6.0.2'
 
 # Timeout monitor. We add two channels to the engine
 # to which cherrypy.Application will publish.
