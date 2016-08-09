80e89ee1f0073af35add817896b1a4a979ede922
Safihre <safihre@sabnzbd.org>
2016-08-09 15:15:29 +0200

    Add index to history-API so Glitter sorting isn't stupid (#648)

--- sabnzbd/api.py
+++ sabnzbd/api.py
@@ -1876,6 +1876,7 @@ def build_history(start=None, limit=None, verbose=False, verbose_list=None, sear
     items.reverse()
 
     retry_folders = []
+    n = 0
     for item in items:
         for key in item:
             value = item[key]
@@ -1915,6 +1916,10 @@ def build_history(start=None, limit=None, verbose=False, verbose_list=None, sear
             rating = Rating.do.get_rating_by_nzo(item['nzo_id'])
         else:
             rating = None
+
+        item['index'] = n
+        n += 1
+
         item['has_rating'] = rating is not None
         if rating:
             item['rating_avg_video'] = rating.avg_video
