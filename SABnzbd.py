2968b3c30e696cf9394ba37276dd7ee7fc778c1c
Safihre <safihre@sabnzbd.org>
2016-10-07 12:54:42 +0200

    Change par2-classic to par2cmdline on Windows
    Closes #702

--- SABnzbd.py
+++ SABnzbd.py
@@ -480,7 +480,7 @@ def print_modules():
         logging.error(T('par2 binary... NOT found!'))
 
     if sabnzbd.newsunpack.PAR2C_COMMAND:
-        logging.info("par2-classic binary... found (%s)", sabnzbd.newsunpack.PAR2C_COMMAND)
+        logging.info("par2cmdline binary... found (%s)", sabnzbd.newsunpack.PAR2C_COMMAND)
 
     if sabnzbd.newsunpack.RAR_COMMAND:
         logging.info("UNRAR binary... found (%s)", sabnzbd.newsunpack.RAR_COMMAND)
