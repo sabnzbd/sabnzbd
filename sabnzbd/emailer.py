dbe8e64b9e1577aa2e7e1eea2b524e268bdabd8c
Safihre <safihre@sabnzbd.org>
2016-12-02 15:08:22 +0100

    ssmtplib is part of Python 2.7

--- sabnzbd/emailer.py
+++ sabnzbd/emailer.py
@@ -19,7 +19,6 @@
 sabnzbd.emailer - Send notification emails
 """
 
-from sabnzbd.utils import ssmtplib
 import smtplib
 import os
 import logging
@@ -80,7 +79,7 @@ def send(message, email_to, test=None):
         logging.debug("Connecting to server %s:%s", server, port)
 
         try:
-            mailconn = ssmtplib.SMTP_SSL(server, port)
+            mailconn = smtplib.SMTP_SSL(server, port)
             mailconn.ehlo()
 
             logging.debug("Connected to server %s:%s", server, port)
