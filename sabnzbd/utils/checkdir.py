c69e0ccfb55fb9dc2312027e2cf581278201aae4
SanderJo <sander.jonkers+github@gmail.com>
2017-03-16 21:28:56 +0100

    Everything in a try/except as we're dealing with OS calls.

--- sabnzbd/utils/checkdir.py
+++ sabnzbd/utils/checkdir.py
@@ -10,96 +10,101 @@ debug = False
 def isFAT(dir):
 
 # Check if "dir" is on FAT. FAT considered harmful (for big files)
-# Works for Linux, Windows
+# Works for Linux, Windows, MacOS
 # NB: On Windows, full path with drive letter is needed!
 
     FAT = False    # default: not FAT
-    if 'linux' in sys.platform:
-        # On Linux:
-        # df -T /home/sander/weg
+    # We're dealing with OS calls, so put everything in a try/except, just in case:
+    try:
+        if 'linux' in sys.platform:
+            # On Linux:
+            # df -T /home/sander/weg
 
-        '''
-        Example output of a 500GB external USB drive formatted with FAT:
-        $ df -T /media/sander/INTENSO
-        Filesystem     Type 1K-blocks      Used Available Use% Mounted on
-        /dev/sda1      vfat 488263616 163545248 324718368  34% /media/sander/INTENSO
-        '''
+            '''
+            Example output of a 500GB external USB drive formatted with FAT:
+            $ df -T /media/sander/INTENSO
+            Filesystem     Type 1K-blocks      Used Available Use% Mounted on
+            /dev/sda1      vfat 488263616 163545248 324718368  34% /media/sander/INTENSO
+            '''
 
-        cmd = "df -T " + dir + " 2>&1"
-        for thisline in os.popen(cmd).readlines():
-            #print thisline
-            if thisline.find('/')==0:
-                # Starts with /, so a real, local device
-                fstype = thisline.split()[1]
-                if debug: print "File system type:", fstype
-                if fstype.lower().find('fat') >= 0:
-                    FAT = True
-                    if debug: print "FAT found"
-                    break
-    elif 'win32' in sys.platform:
-        dir = dir.upper()    # input could be in lower case like "e:\", so to upper first
-        # On Windows:
-        '''
-        C:\>wmic logicaldisk get name,filesystem
-        FileSystem  Name
-        NTFS        C:
-                D:
-        FAT         E:
-        FAT         F:
-        NTFS        G:
-        NTFS        H:
-        NTFS        M:
-        NTFS        Q:
-        NTFS        X:
-
-        We now can see dir "E:\other\dir\something" is on FAT (just like F:\bla),
-        and "Q:\dome\dir\bla" is not on FAT.
-        '''
-        windowscmd = "wmic logicaldisk get name,filesystem".split()
-        CREATE_NO_WINDOW = 0x08000000
-        for line in subprocess.check_output(windowscmd,creationflags=CREATE_NO_WINDOW).replace('\r','').split('\n'):
-            if 'FAT' in line:
-                # I'm quite sure if 'FAT' is there, there is always a space before the drive letter,
-                # and thus a split() is possible, but to be sure put it in a try/except:
-                try:
-                    driveletter = line.split()[-1]
-                    # We have found a drive letter which is FAT
-                    # Now check if that drive letter is in the dir to be checked (somewhere in the beginning of the path)
-                    # ... because can be '\\?\C:\Media\...'
-                    if dir.find(driveletter) >=0 and dir.find(driveletter) <= 5 :
+            cmd = "df -T " + dir + " 2>&1"
+            for thisline in os.popen(cmd).readlines():
+                #print thisline
+                if thisline.find('/')==0:
+                    # Starts with /, so a real, local device
+                    fstype = thisline.split()[1]
+                    if debug: print "File system type:", fstype
+                    if fstype.lower().find('fat') >= 0:
                         FAT = True
-                        break    # we're done
-                except:
-                    continue
-    elif 'darwin' in sys.platform:
-        # MacOS formerly known as OSX
-        '''
-        MacOS needs a two-step approach:
+                        if debug: print "FAT found"
+                        break
+        elif 'win32' in sys.platform:
+            dir = dir.upper()    # input could be in lower case like "e:\", so to upper first
+            # On Windows:
+            '''
+            C:\>wmic logicaldisk get name,filesystem
+            FileSystem  Name
+            NTFS        C:
+                        D:
+            FAT         E:
+            FAT         F:
+            NTFS        G:
+            NTFS        H:
+            NTFS        M:
+            NTFS        Q:
+            NTFS        X:
 
-        server:~ sander$ df /Volumes/CARTUNES/Tuna/
-        Filesystem   512-blocks      Used Available Capacity iused ifree %iused  Mounted on
-        /dev/disk9s1  120815744 108840000  11975744    91%       0     0  100%   /Volumes/CARTUNES
+            We now can see dir "E:\other\dir\something" is on FAT (just like F:\bla),
+            and "Q:\dome\dir\bla" is not on FAT.
+            '''
+            windowscmd = "wmic logicaldisk get name,filesystem".split()
+            CREATE_NO_WINDOW = 0x08000000
+            for line in subprocess.check_output(windowscmd,creationflags=CREATE_NO_WINDOW).replace('\r','').split('\n'):
+                if 'FAT' in line:
+                    # I'm quite sure if 'FAT' is there, there is always a space before the drive letter,
+                    # and thus a split() is possible, but to be sure put it in a try/except:
+                    try:
+                        driveletter = line.split()[-1]
+                        # We have found a drive letter which is FAT
+                        # Now check if that drive letter is in the dir to be checked (somewhere in the beginning of the path)
+                        # ... because can be '\\?\C:\Media\...'
+                        if dir.find(driveletter) >=0 and dir.find(driveletter) <= 5 :
+                            FAT = True
+                            break    # we're done
+                    except:
+                        continue
+        elif 'darwin' in sys.platform:
+            # MacOS formerly known as OSX
+            '''
+            MacOS needs a two-step approach:
+            
+            # First: directory => device
+            server:~ sander$ df /Volumes/CARTUNES/Tuna/
+            Filesystem   512-blocks      Used Available Capacity iused ifree %iused  Mounted on
+            /dev/disk9s1  120815744 108840000  11975744    91%       0     0  100%   /Volumes/CARTUNES
+            
+            # Then: device => filesystem type
+            server:~ sander$ mount | grep /dev/disk9s1
+            /dev/disk9s1 on /Volumes/CARTUNES (msdos, local, nodev, nosuid, noowners)
 
-        server:~ sander$ mount | grep /dev/disk9s1
-        /dev/disk9s1 on /Volumes/CARTUNES (msdos, local, nodev, nosuid, noowners)
 
+            '''
+            dfcmd = "df " + dir
+            device = ''
+            for thisline in os.popen(dfcmd).readlines():
+                if thisline.find('/')==0:
+                    if debug: print thisline
+                    # Starts with /, so a real, local device
+                    device = thisline.split()[0]
+                    mountcmd = "mount | grep " + device
+                    mountoutput = os.popen(mountcmd).readline().strip()
+                    if debug: print mountoutput
+                    if 'msdos' in mountoutput.split('(')[1]:
+                        FAT = True
+                    break
 
-        '''
-        dfcmd = "df " + dir
-        device = ''
-        for thisline in os.popen(dfcmd).readlines():
-            if thisline.find('/')==0:
-                if debug: print thisline
-                # Starts with /, so a real, local device
-                device = thisline.split()[0]
-                mountcmd = "mount | grep " + device
-                mountoutput = os.popen(mountcmd).readline().strip()
-                if debug: print mountoutput
-                if 'msdo' in mountoutput.split('(')[1]:
-                    FAT = True
-                break
-
-
+    except:
+        pass
     return FAT
 
 
