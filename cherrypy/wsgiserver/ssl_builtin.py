b1e3195c74712e93140c323508e0c276255c5fcb
Jonathon Saine <thezoggy@gmail.com>
2017-03-21 06:05:09 -0500

    Fixes: #860 Handle: `SSLError: [SSL: HTTPS_PROXY_REQUEST] https proxy request (_ssl.c:661)`

--- cherrypy/wsgiserver/ssl_builtin.py
+++ cherrypy/wsgiserver/ssl_builtin.py
@@ -86,7 +86,7 @@ class BuiltinSSLAdapter(wsgiserver.SSLAdapter):
                 # Check if it's one of the known errors
                 # Errors that are caught by PyOpenSSL, but thrown by built-in ssl
                 _block_errors = ('unknown protocol', 'unknown ca', 'unknown_ca', 'unknown error',
-                                 'inappropriate fallback', 'wrong version number',
+                                 'https proxy request', 'inappropriate fallback', 'wrong version number',
                                  'no shared cipher', 'certificate unknown', 'ccs received early')
                 for error_text in _block_errors:
                     if error_text in e.args[1].lower():
