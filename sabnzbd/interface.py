6393dc0dca17151a965829fd74509915f00b89fa
Safihre <safihre@sabnzbd.org>
2017-07-23 10:52:37 +0200

    Remove history_limit from Specials

--- sabnzbd/interface.py
+++ sabnzbd/interface.py
@@ -1384,7 +1384,7 @@ SPECIAL_BOOL_LIST = \
      )
 SPECIAL_VALUE_LIST = \
     ('size_limit', 'folder_max_length', 'fsys_type', 'movie_rename_limit', 'nomedia_marker',
-              'req_completion_rate', 'wait_ext_drive', 'history_limit', 'show_sysload',
+              'req_completion_rate', 'wait_ext_drive', 'show_sysload',
               'direct_unpack_threads', 'ipv6_servers', 'selftest_host', 'rating_host'
      )
 SPECIAL_LIST_LIST = ('rss_odd_titles', 'quick_check_ext_ignore')
