5931beaa5c58e4ad66445947c63ea4c698a7e782
Safihre <safihre@sabnzbd.org>
2017-11-20 21:10:58 +0100

    Quote all arguments to external processes on Windows

--- sabnzbd/newsunpack.py
+++ sabnzbd/newsunpack.py
@@ -2333,10 +2333,8 @@ def list2cmdline(lst):
     for arg in lst:
         if not arg:
             nlst.append('""')
-        elif (' ' in arg) or ('\t' in arg) or ('&' in arg) or ('|' in arg) or (';' in arg) or (',' in arg):
-            nlst.append('"%s"' % arg)
         else:
-            nlst.append(arg)
+            nlst.append('"%s"' % arg)
     return ' '.join(nlst)
 
 
