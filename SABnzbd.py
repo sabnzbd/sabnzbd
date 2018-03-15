2cdf284578f7c65f0f75e92c3d8d61054fd56266
Safihre <safihre@sabnzbd.org>
2018-03-15 13:13:01 +0100

    Update copyright to 2018

--- SABnzbd.py
+++ SABnzbd.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python -OO
-# Copyright 2008-2017 The SABnzbd-Team <team@sabnzbd.org>
+# Copyright 2007-2018 The SABnzbd-Team <team@sabnzbd.org>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -216,7 +216,7 @@ def print_version():
     print """
 %s-%s
 
-Copyright (C) 2008-2017, The SABnzbd-Team <team@sabnzbd.org>
+Copyright (C) 2007-2018, The SABnzbd-Team <team@sabnzbd.org>
 SABnzbd comes with ABSOLUTELY NO WARRANTY.
 This is free software, and you are welcome to redistribute it
 under certain conditions. It is licensed under the
