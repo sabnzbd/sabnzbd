e2761d967ec02af50969b32543211cb01ffb6a8f
Safihre <safihre@sabnzbd.org>
2018-08-27 11:11:26 +0200

    Functional testing using Selenium

--- tests/testhelper.py
+++ tests/testhelper.py
@@ -19,12 +19,19 @@
 tests.testhelper - Basic helper functions
 """
 
-import urllib2
-import json
+import os
+import shutil
+import subprocess
+import time
+
 import requests
+from selenium.webdriver.support.ui import WebDriverWait
 
 SAB_HOST = 'localhost'
 SAB_PORT = 8081
+SAB_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SAB_CACHE_DIR = os.path.join(SAB_BASE_DIR, 'cache')
+SAB_COMPLETE_DIR = os.path.join(SAB_CACHE_DIR, 'Downloads', 'complete')
 
 
 def get_url_result(url=''):
@@ -41,8 +48,41 @@ def get_api_result(mode, extra_arguments={}):
     return r.json()
 
 
-def upload_nzb(file):
+def upload_nzb(filename):
     """ Upload file and return nzo_id reponse """
-    files = {'name': open(file, 'rb')}
-    arguments ={'apikey':'apikey', 'mode':'addfile', 'output': 'json'}
+    files = {'name': open(filename, 'rb')}
+    arguments = {'apikey': 'apikey', 'mode': 'addfile', 'output': 'json'}
     return requests.post('http://%s:%s/api' % (SAB_HOST, SAB_PORT), files=files, data=arguments).json()
+
+
+def setUpModule():
+    # Remove cache if already there
+    if os.path.isdir(SAB_CACHE_DIR):
+        shutil.rmtree(SAB_CACHE_DIR)
+
+    # Copy basic config file with API key
+    os.mkdir(SAB_CACHE_DIR)
+    shutil.copyfile(os.path.join(SAB_BASE_DIR, 'sabnzbd.basic.ini'), os.path.join(SAB_CACHE_DIR, 'sabnzbd.ini'))
+
+    # Check if we have language files
+    if not os.path.exists(os.path.join(SAB_BASE_DIR, '..', 'locale')):
+        lang_command = 'python %s/../tools/make_mo.py' % SAB_BASE_DIR
+        subprocess.Popen(lang_command.split())
+
+    # Start SABnzbd
+    sab_command = 'python %s/../SABnzbd.py --new -l2 -s %s:%s -b0 -f %s' % (SAB_BASE_DIR, SAB_HOST, SAB_PORT, SAB_CACHE_DIR)
+    subprocess.Popen(sab_command.split())
+    time.sleep(10)
+
+
+def tearDownModule():
+    # Gracefull shutdown request
+    get_url_result('shutdown')
+    # Takes a second to shutdown
+    for x in range(10):
+        try:
+            shutil.rmtree(SAB_CACHE_DIR)
+            break
+        except OSError:
+            print "Unable to remove cache dir (try %d)" % x
+            time.sleep(1)
