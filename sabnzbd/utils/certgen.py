88770ae6eb19211a37f5fc68eca69644d7608ec1
Safihre <safihre@sabnzbd.org>
2018-12-26 22:33:47 +0100

    Py3: Fix certgen and typos in servertests

--- sabnzbd/utils/certgen.py
+++ sabnzbd/utils/certgen.py
@@ -11,27 +11,14 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography import x509
 from cryptography.x509.oid import NameOID
 import datetime
-import os
 import socket
 
 from sabnzbd.getipaddress import localipv4
 
-# Ported from cryptography/utils.py
-def int_from_bytes(data, byteorder, signed=False):
-    assert byteorder == 'big'
-    assert not signed
-
-    # call bytes() on data to allow the use of bytearrays
-    return int(bytes(data).encode('hex'), 16)
-
-
-# Ported from cryptography/x509/base.py
-def random_serial_number():
-    return int_from_bytes(os.urandom(20), "big") >> 1
-
-
-# Ported from cryptography docs/x509/tutorial.rst (set with no encryption)
 def generate_key(key_size=2048, output_file='key.pem'):
+    """ Generate the private-key file for the self-signed certificate
+        Ported from cryptography docs/x509/tutorial.rst (set with no encryption)
+    """
     # Generate our key
     private_key = rsa.generate_private_key(
         public_exponent=65537,
@@ -51,8 +38,10 @@ def generate_key(key_size=2048, output_file='key.pem'):
     return private_key
 
 
-# Ported from cryptography docs/x509/tutorial.rst
 def generate_local_cert(private_key, days_valid=3560, output_file='cert.cert', LN='SABnzbd', ON='SABnzbd'):
+    """ Generate a certificate, using basic information.
+        Ported from cryptography docs/x509/tutorial.rst
+    """
     # Various details about who we are. For a self-signed certificate the
     # subject and issuer are always the same.
     subject = issuer = x509.Name([
@@ -60,7 +49,6 @@ def generate_local_cert(private_key, days_valid=3560, output_file='cert.cert', L
         x509.NameAttribute(NameOID.ORGANIZATION_NAME, ON),
         # x509.NameAttribute(NameOID.COMMON_NAME, CN),
     ])
-
 
     # build Subject Alternate Names (aka SAN) list
     # First the host names, add with x509.DNSName():
@@ -91,7 +79,7 @@ def generate_local_cert(private_key, days_valid=3560, output_file='cert.cert', L
     ).not_valid_after(
         datetime.datetime.utcnow() + datetime.timedelta(days=days_valid)
     ).serial_number(
-        random_serial_number()
+        x509.random_serial_number()
     ).add_extension(
         x509.SubjectAlternativeName(san_list),
         critical=True,
--- sabnzbd/utils/servertests.py
+++ sabnzbd/utils/servertests.py
@@ -84,7 +84,7 @@ def test_nntp_server(host, port, server=None, username=None, password=None, ssl=
         while not nw.connected:
             nw.clear_data()
             nw.recv_chunk(block=True)
-            #more ssl related: handle 1/n-1 splitting to prevent Rizzo/Duong-Beast
+            # Handle 1/n-1 splitting to prevent Rizzo/Duong-Beast
             read_sockets, _, _ = select.select([nw.nntp.sock], [], [], 0.1)
             if read_sockets:
                 nw.recv_chunk(block=True)
