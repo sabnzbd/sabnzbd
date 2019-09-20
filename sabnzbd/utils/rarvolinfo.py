4a24d9d64c9c7ae7491be14b37a888d9fb92b01a
Safihre <safihre@sabnzbd.org>
2019-09-20 16:01:46 +0200

    Integrate get_rar_extension in SABnzbd

--- sabnzbd/utils/rarvolinfo.py
+++ sabnzbd/utils/rarvolinfo.py
@@ -25,71 +25,19 @@ import os
 
 try:
     import sabnzbd.utils.rarfile as rarfile
-except:
+except ImportError:
     import rarfile
-
-
-def get_rar_volume_number(myrarfile):
-    """
-    Find out volume number of a rar file. Returns < 0 in case of file problems
-    """
-    volumenumber = -1  # default aka something bad
-
-    if not os.path.isfile(myrarfile):
-        return volumenumber
-
-    try:
-        rar_ver = rarfile.is_rarfile(myrarfile)
-        with open(myrarfile, "rb") as fh:
-            if rar_ver.endswith("3"):
-                # At the end of the file, we need about 20 bytes
-                fh.seek(-20, os.SEEK_END)
-                mybuf = fh.read()
-                volumenumber = 1 + mybuf[-9] + 256 * mybuf[-8]
-
-            elif rar_ver.endswith("5"):
-                mybuf = fh.read(100)  # first 100 bytes is enough
-
-                # Get (and skip) the first 8 + 4 bytes
-                rar5sig, newpos = rarfile.load_bytes(mybuf, 8, 0)  # Rar5 signature
-                crc32, newpos = rarfile.load_bytes(mybuf, 4, newpos)  # crc32
-
-                # Then get the VINT values (with variable size, so parse them all):
-                headersize, newpos = rarfile.load_vint(mybuf, newpos)
-                headertype, newpos = rarfile.load_vint(mybuf, newpos)
-                headerflags, newpos = rarfile.load_vint(mybuf, newpos)
-                extraareasize, newpos = rarfile.load_vint(mybuf, newpos)
-                archiveflags, newpos = rarfile.load_vint(mybuf, newpos)
-
-                # Now we're ready for the volume number:
-                if archiveflags & 2:
-                    value, newpos = rarfile.load_vint(mybuf, newpos)
-                    volumenumber = value + 1
-                else:
-                    # first volume, aka 1
-                    volumenumber = 1
-    except:
-        volumenumber = -2
-        pass
-
-    return volumenumber
-
-
-def int2zeropaddedstring(myint, digits=3):
-    zeropaddedstring = ("0000" + str(myint))[-digits:]
-    return zeropaddedstring
 
 
 def get_rar_extension(myrarfile):
     """
     Find out orginal extension of a rar file. Returns "" in case of file problems
-    So ... returns:     "part001.rar", ... "part005.rar" 
-    or old number scheme (can only happen for rar3/rar4 files): "rar", r00, ... r89 
+    So ... returns:     "part001.rar", ... "part005.rar"
+    or old number scheme (can only happen for rar3/rar4 files): "rar", r00, ... r89
     """
-    org_extension = ""  # default aka something bad
-
-    if not os.path.isfile(myrarfile):
-        return org_extension
+    # When things go wrong
+    volumenumber = -1
+    org_extension = False
 
     try:
         rar_ver = rarfile.is_rarfile(myrarfile)
@@ -106,13 +54,13 @@ def get_rar_extension(myrarfile):
                 volumenumber = 1 + mybuf[-9] + 256 * mybuf[-8]
 
                 if newnumbering:
-                    org_extension = "part" + int2zeropaddedstring(volumenumber) + ".rar"
+                    org_extension = "part%03d.rar" % volumenumber
                 else:
                     # 1, 2, 3, 4 resp refers to .rar, .r00, .r01, .r02 ...
                     if volumenumber == 1:
                         org_extension = "rar"
                     else:
-                        org_extension = "r" + int2zeropaddedstring(volumenumber - 2)
+                        org_extension = "r%03d" % (volumenumber - 2)
 
             elif rar_ver.endswith("5"):
                 mybuf = fh.read(100)  # first 100 bytes is enough
@@ -137,12 +85,11 @@ def get_rar_extension(myrarfile):
                     volumenumber = 1
 
                 # Combine into the extension
-                org_extension = "part" + int2zeropaddedstring(volumenumber) + ".rar"
+                org_extension = "part%03d.rar" % volumenumber
     except:
-        org_extension = "exception"
         pass
 
-    return org_extension
+    return volumenumber, org_extension
 
 
 # Main
@@ -152,7 +99,6 @@ if __name__ == "__main__":
     try:
         myfile = sys.argv[1]
         print("File:", myfile)
-        print("Volume number:", get_rar_volume_number(myfile))
-        print("Extension:", get_rar_extension(myfile))
+        print("Volume and extension:", get_rar_extension(myfile))
     except:
         print("Please specify rar file as parameter")
