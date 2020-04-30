ea73ade1d3edc3aaa383ebdab11c153d90cc64f8
Safihre <safihre@sabnzbd.org>
2020-04-30 15:57:03 +0200

    Use threadpool as context to close resource properly

--- sabnzbd/getipaddress.py
+++ sabnzbd/getipaddress.py
@@ -39,10 +39,10 @@ def timeout(max_timeout):
         @functools.wraps(item)
         def func_wrapper(*args, **kwargs):
             """ Closure for function. """
-            pool = multiprocessing.pool.ThreadPool(processes=1)
-            async_result = pool.apply_async(item, args, kwargs)
-            # raises a TimeoutError if execution exceeds max_timeout
-            return async_result.get(max_timeout)
+            with multiprocessing.pool.ThreadPool(processes=1) as pool:
+                async_result = pool.apply_async(item, args, kwargs)
+                # raises a TimeoutError if execution exceeds max_timeout
+                return async_result.get(max_timeout)
 
         return func_wrapper
 
