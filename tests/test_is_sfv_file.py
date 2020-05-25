f72fe2b6636996b93d916a334b34b41c2b97588c
Sander Jonkers <sander.jonkers+github@gmail.com>
2020-05-25 21:14:33 +0200

    is_sfv_file(): pytest according to black

--- tests/test_is_sfv_file.py
+++ tests/test_is_sfv_file.py
@@ -16,15 +16,13 @@ class Test_is_sfv_file:
     """
 
     def test_valid_unicode_sfv(self):
-        assert is_sfv_file('tests/data/good_sfv_unicode.sfv')
+        assert is_sfv_file("tests/data/good_sfv_unicode.sfv")
 
     def test_valid_one_line_sfv(self):
-        assert is_sfv_file('tests/data/one_line.sfv')
+        assert is_sfv_file("tests/data/one_line.sfv")
 
     def test_only_comments(self):
-        assert not is_sfv_file('tests/data/only_comments.sfv')
+        assert not is_sfv_file("tests/data/only_comments.sfv")
 
-       
     def test_random_bin(self):
-        assert not is_sfv_file('tests/data/random.bin')        
-        
+        assert not is_sfv_file("tests/data/random.bin")
