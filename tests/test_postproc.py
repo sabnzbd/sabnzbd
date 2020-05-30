707429f3e5182b9c69bb5b9140c8569a8a7684fe
Sander Jonkers <sander.jonkers+github@gmail.com>
2020-05-30 13:43:26 +0200

    pytest: postproc -> rar_renamer(): check on matching filenames

--- tests/test_postproc.py
+++ tests/test_postproc.py
@@ -19,13 +19,13 @@ class TestPostProc:
     def test_rar_renamer(self):
 
         # Function to deobfuscate one directorty with rar_renamer()
-        def deobfuscate_dir(sourcedir, expected_extensions):
+        def deobfuscate_dir(sourcedir, expected_filename_matches):
             # sourcedir is the relative path to the directory with obfuscated files
 
             # enrich to absolute path:
             sourcedir = os.path.join(os.getcwd(), sourcedir)
             # We create a workingdir inside the sourcedir, because the filenames are really changed
-            workingdir = os.path.join(sourcedir, "WORKING-DIR")
+            workingdir = os.path.join(sourcedir, "working-dir")
             # if workingdir is still there from previous run, remove it:
             if os.path.isdir(workingdir):
                 try:
@@ -44,9 +44,9 @@ class TestPostProc:
             number_renamed_files = rar_renamer(nzo, workingdir)
 
             # run check on the resulting files
-            for extension in expected_extensions:
-                if len(globber_full(workingdir, "*" + extension)) != expected_extensions[extension]:
-                    pytest.fail("Fail on checking extensions {}".format(extension))
+            for filename_match in expected_filename_matches:
+                if len(globber_full(workingdir, filename_match)) != expected_filename_matches[filename_match]:
+                    pytest.fail("Fail on checking filename_matchs {}".format(workingdir,filename_match))
 
             # Remove workingdir again
             try:
@@ -57,11 +57,11 @@ class TestPostProc:
             return number_renamed_files
 
         # The tests and asserts per directory:
-        expected_extensions = { "part007.rar": 1}
-        assert deobfuscate_dir("tests/data/obfuscated_single_rar_set/", expected_extensions) == 7
+        expected_filename_matches = { "*part007.rar": 1, "*-*-*-*-*": 0}
+        assert deobfuscate_dir("tests/data/obfuscated_single_rar_set/", expected_filename_matches) == 7
 
-        expected_extensions = { "part007.rar": 2,  "part009.rar": 1}
-        assert deobfuscate_dir("tests/data/obfuscated_two_rar_sets", expected_extensions) == 16
+        expected_filename_matches = { "*part007.rar": 2,  "*part009.rar": 1, "c2bfeeb1-a0b6-47d2-be35-50328927c1ae": 0}
+        assert deobfuscate_dir("tests/data/obfuscated_two_rar_sets", expected_filename_matches) == 16
 
-        expected_extensions = { "part001.rar": 0}
-        assert deobfuscate_dir("tests/data/obfuscated_but_no_rar", expected_extensions) == 0
+        expected_filename_matches = { "*.rar": 0, "*-*-*-*-*": 6}
+        assert deobfuscate_dir("tests/data/obfuscated_but_no_rar", expected_filename_matches) == 0
