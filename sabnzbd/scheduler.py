sabnzbd/sabnzbd

commit 3259f83b9d9fdb9b1f70e2ca79c5d827c7c36a27

Do not use variable names that shadow built-in ones
Maybe fixes https://forums.sabnzbd.org/viewtopic.php?f=11&t=24785

sabnzbd/scheduler.py
@@ -458,9 +458,9 @@ def pause_int():
             val = abs(val)
         else:
             sign = ''
-        min = int(val / 60)
+        mins = int(val / 60)
         sec = int(val - min * 60)
-        return "%s%d:%02d" % (sign, min, sec)
+        return "%s%d:%02d" % (sign, mins, sec)
 
 
 def pause_check():

SABnzbd.py
@@ -715,13 +715,13 @@ def commandline_handler():
     if len(sys.argv) > 1 and \
        'sabnzbd' in sys.argv[1].lower() and \
        not sys.argv[1].startswith('-'):
-        slice = 2
+        slice_start = 2
     else:
-        slice = 1
+        slice_start = 1
 
     # Prepend options from env-variable to options
     info = os.environ.get('SABnzbd', '').split()
-    info.extend(sys.argv[slice:])
+    info.extend(sys.argv[slice_start:])
 
     try:
         opts, args = getopt.getopt(info, "phdvncwl:s:f:t:b:2:",

sabnzbd/api.py
@@ -663,7 +663,7 @@ def _api_osx_icon(name, output, kwargs):
 
 def _api_rescan(name, output, kwargs):
     """ API: accepts output """
-    NzbQueue.do.scan_jobs(all=False, action=True)
+    NzbQueue.do.scan_jobs(all_jobs=False, action=True)
     return report(output)
 
 
@@ -1159,7 +1159,7 @@ def build_status(skip_dashboard=False, output=None):
     info['logfile'] = sabnzbd.LOGFILE
     info['weblogfile'] = sabnzbd.WEBLOGFILE
     info['loglevel'] = str(cfg.log_level())
-    info['folders'] = NzbQueue.do.scan_jobs(all=False, action=False)
+    info['folders'] = NzbQueue.do.scan_jobs(all_jobs=False, action=False)
     info['configfn'] = config.get_filename()
 
     # Dashboard: Speed of System
@@ -1282,13 +1282,13 @@ def build_queue(start=0, limit=0, trans=False, output=None, search=None):
     for pnfo in pnfo_list:
         nzo_id = pnfo.nzo_id
         bytesleft = pnfo.bytes_left
-        bytes = pnfo.bytes
+        bytes_total = pnfo.bytes
         average_date = pnfo.avg_date
         is_propagating = (pnfo.avg_stamp + float(cfg.propagation_delay() * 60)) > time.time()
         status = pnfo.status
         priority = pnfo.priority
         mbleft = (bytesleft / MEBI)
-        mb = (bytes / MEBI)
+        mb = (bytes_total / MEBI)
 
         slot = {}
         slot['index'] = n
@@ -1302,7 +1302,7 @@ def build_queue(start=0, limit=0, trans=False, output=None, search=None):
         slot['cat'] = pnfo.category if pnfo.category else 'None'
         slot['mbleft'] = "%.2f" % mbleft
         slot['mb'] = "%.2f" % mb
-        slot['size'] = format_bytes(bytes)
+        slot['size'] = format_bytes(bytes_total)
         slot['sizeleft'] = format_bytes(bytesleft)
         slot['percentage'] = "%s" % (int(((mb - mbleft) / mb) * 100)) if mb != mbleft else '0'
         slot['mbmissing'] = "%.2f" % (pnfo.bytes_missing / MEBI)
@@ -1593,14 +1593,14 @@ def build_queue_header(search=None, start=0, limit=0, output=None):
     qnfo = NzbQueue.do.queue_info(search=search, start=start, limit=limit)
 
     bytesleft = qnfo.bytes_left
-    bytes = qnfo.bytes
+    bytes_total = qnfo.bytes
 
     header['kbpersec'] = "%.2f" % (bytespersec / KIBI)
     header['speed'] = to_units(bytespersec)
     header['mbleft'] = "%.2f" % (bytesleft / MEBI)
-    header['mb'] = "%.2f" % (bytes / MEBI)
+    header['mb'] = "%.2f" % (bytes_total / MEBI)
     header['sizeleft'] = format_bytes(bytesleft)
-    header['size'] = format_bytes(bytes)
+    header['size'] = format_bytes(bytes_total)
     header['noofslots_total'] = qnfo.q_fullsize
 
     if Downloader.do.paused or Downloader.do.postproc:

sabnzbd/assembler.py
@@ -144,11 +144,11 @@ class Assembler(Thread):
                         # Parse par2 files, cloaked or not
                         nzo.handle_par2(nzf, filepath)
 
-                    filter, reason = nzo_filtered_by_rating(nzo)
-                    if filter == 1:
+                    filter_output, reason = nzo_filtered_by_rating(nzo)
+                    if filter_output == 1:
                         logging.warning(remove_warning_label(T('WARNING: Paused job "%s" because of rating (%s)')), nzo.final_name, reason)
                         nzo.pause()
-                    elif filter == 2:
+                    elif filter_output == 2:
                         logging.warning(remove_warning_label(T('WARNING: Aborted job "%s" because of rating (%s)')), nzo.final_name, reason)
                         nzo.fail_msg = T('Aborted, rating filter matched (%s)') % reason
                         sabnzbd.nzbqueue.NzbQueue.do.end_job(nzo)

sabnzbd/downloader.py
@@ -56,10 +56,10 @@ TIMER_LOCK = RLock()
 
 class Server:
 
-    def __init__(self, id, displayname, host, port, timeout, threads, priority, ssl, ssl_verify, ssl_ciphers,
+    def __init__(self, server_id, displayname, host, port, timeout, threads, priority, ssl, ssl_verify, ssl_ciphers,
                  send_group, username=None, password=None, optional=False, retention=0):
 
-        self.id = id
+        self.id = server_id
         self.newid = None
         self.restart = False
         self.displayname = displayname
@@ -368,7 +368,7 @@ class Downloader(Thread):
 
             # Remove all connections to server
             for nw in server.idle_threads + server.busy_threads:
-                self.__reset_nw(nw, "forcing disconnect", warn=False, wait=False, quit=False)
+                self.__reset_nw(nw, "forcing disconnect", warn=False, wait=False, send_quit=False)
             # Make sure server address resolution is refreshed
             server.info = None
 
@@ -501,8 +501,8 @@ class Downloader(Thread):
             if self.force_disconnect:
                 for server in self.servers:
                     for nw in server.idle_threads + server.busy_threads:
-                        quit = nw.connected and server.active
-                        self.__reset_nw(nw, "forcing disconnect", warn=False, wait=False, quit=quit)
+                        send_quit = nw.connected and server.active
+                        self.__reset_nw(nw, "forcing disconnect", warn=False, wait=False, send_quit=send_quit)
                     # Make sure server address resolution is refreshed
                     server.info = None
 
@@ -617,7 +617,7 @@ class Downloader(Thread):
                                     if server.errormsg != errormsg:
                                         server.errormsg = errormsg
                                         logging.warning(T('Too many connections to server %s'), server.host)
-                                    self.__reset_nw(nw, None, warn=False, destroy=True, quit=True)
+                                    self.__reset_nw(nw, None, warn=False, destroy=True, send_quit=True)
                                     self.plan_server(server, _PENALTY_TOOMANY)
                                     server.threads -= 1
                             elif ecode in (502, 481, 482) and clues_too_many_ip(msg):
@@ -672,7 +672,7 @@ class Downloader(Thread):
                                     if penalty and (block or server.optional):
                                         self.plan_server(server, penalty)
                                     sabnzbd.nzbqueue.NzbQueue.do.reset_all_try_lists()
-                                self.__reset_nw(nw, None, warn=False, quit=True)
+                                self.__reset_nw(nw, None, warn=False, send_quit=True)
                             continue
                         except:
                             logging.error(T('Connecting %s@%s failed, message=%s'),
@@ -734,7 +734,7 @@ class Downloader(Thread):
                 return f
         return None
 
-    def __reset_nw(self, nw, reset_msg, warn=True, wait=True, destroy=False, quit=False):
+    def __reset_nw(self, nw, reset_msg, warn=True, wait=True, destroy=False, send_quit=False):
         server = nw.server
         article = nw.article
         fileno = None
@@ -772,9 +772,9 @@ class Downloader(Thread):
                 sabnzbd.nzbqueue.NzbQueue.do.reset_try_lists(article)
 
         if destroy:
-            nw.terminate(quit=quit)
+            nw.terminate(quit=send_quit)
         else:
-            nw.hard_reset(wait, quit=quit)
+            nw.hard_reset(wait, send_quit=send_quit)
 
         # Empty SSL info, it might change on next connect
         server.ssl_info = ''
@@ -797,11 +797,11 @@ class Downloader(Thread):
                 self.read_fds[fileno] = nw
         except socket.error as err:
             logging.info('Looks like server closed connection: %s', err)
-            self.__reset_nw(nw, "server broke off connection", quit=False)
+            self.__reset_nw(nw, "server broke off connection", send_quit=False)
         except:
             logging.error(T('Suspect error in downloader'))
             logging.info("Traceback: ", exc_info=True)
-            self.__reset_nw(nw, "server broke off connection", quit=False)
+            self.__reset_nw(nw, "server broke off connection", send_quit=False)
 
     #------------------------------------------------------------------------------
     # Timed restart of servers admin.

sabnzbd/filesystem.py
@@ -569,10 +569,10 @@ def get_unique_filename(path):
 
 
 @synchronized(DIR_LOCK)
-def recursive_listdir(dir):
+def recursive_listdir(input_dir):
     """ List all files in dirs and sub-dirs """
     filelist = []
-    for root, dirs, files in os.walk(dir):
+    for root, dirs, files in os.walk(input_dir):
         for file in files:
             if ".AppleDouble" not in root and ".DS_Store" not in root:
                 p = os.path.join(root, file)

sabnzbd/interface.py
@@ -1059,7 +1059,7 @@ class ConfigPage:
             new[svr] = {}
         conf['servers'] = new
 
-        conf['folders'] = NzbQueue.do.scan_jobs(all=False, action=False)
+        conf['folders'] = NzbQueue.do.scan_jobs(all_jobs=False, action=False)
 
         template = Template(file=os.path.join(sabnzbd.WEB_DIR_CONFIG, 'config.tmpl'),
                             searchList=[conf], compilerSettings=CHEETAH_DIRECTIVES)
@@ -2288,7 +2288,7 @@ def orphan_delete(kwargs):
 
 
 def orphan_delete_all():
-    paths = NzbQueue.do.scan_jobs(all=False, action=False)
+    paths = NzbQueue.do.scan_jobs(all_jobs=False, action=False)
     for path in paths:
         kwargs = {'name': path}
         orphan_delete(kwargs)
@@ -2303,7 +2303,7 @@ def orphan_add(kwargs):
 
 
 def orphan_add_all():
-    paths = NzbQueue.do.scan_jobs(all=False, action=False)
+    paths = NzbQueue.do.scan_jobs(all_jobs=False, action=False)
     for path in paths:
         kwargs = {'name': path}
         orphan_add(kwargs)

sabnzbd/newswrapper.py
@@ -424,10 +424,10 @@ class NewsWrapper:
         self.data = []
         self.last_line = ''
 
-    def hard_reset(self, wait=True, quit=True):
+    def hard_reset(self, wait=True, send_quit=True):
         if self.nntp:
             try:
-                if quit:
+                if send_quit:
                     self.nntp.sock.sendall(b'QUIT\r\n')
                     time.sleep(0.1)
                 self.nntp.sock.close()

commit 63794869a48fcfd22d9dac937894b662598d4b0a

fix variable name that broke pausing (#1500)
this was introduced in 3259f83b9 and broke pausing

Co-authored-by: William Friesen <will@williamfriesen.com>

sabnzbd/scheduler.py
@@ -459,7 +459,7 @@ def pause_int():
         else:
             sign = ''
         mins = int(val / 60)
-        sec = int(val - min * 60)
+        sec = int(val - mins * 60)
         return "%s%d:%02d" % (sign, mins, sec)
 
 