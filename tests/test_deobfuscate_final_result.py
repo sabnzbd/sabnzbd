e1a845c6e58006eeb485c60e6fbe7fbcba65297c
Sander Jonkers <sander.jonkers+github@gmail.com>
2020-07-21 19:58:49 +0200

    Deobfuscate / rename final files: unittests

---
+++ tests/test_deobfuscate_final_result.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3 -OO
+# Copyright 2007-2020 The SABnzbd-Team <team@sabnzbd.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""
+Testing SABnzbd deobfuscate util
+"""
+
+from sabnzbd.utils.deobfuscate import *
+
+
+class TestItAll:
+    def test_is_probably_obfuscated(self):
+        
+        # obfuscated names
+        assert is_probably_obfuscated('599c1c9e2bdfb5114044bf25152b7eaa.mkv')
+        assert is_probably_obfuscated('/my/blabla/directory/stuff/599c1c9e2bdfb5114044bf25152b7eaa.mkv')
+        assert is_probably_obfuscated('/my/blabla/directory/stuff/afgm.avi')
+        
+        
+        # non-obfuscated names:
+        assert not is_probably_obfuscated('/my/blabla/directory/stuff/My Favorite Program S03E04.mkv')
+        assert not is_probably_obfuscated('/my/blabla/directory/stuff/Great Movie (2020).mkv')
+
+    def test_blbla(self):
+        assert True
+
+
