e323c014f9e8328707db0f9150332c6506ee1081
Safihre <safihre@sabnzbd.org>
2020-08-01 16:17:08 +0200

    Set version information to 3.0.0

--- sabnzbd/version.py
+++ sabnzbd/version.py
@@ -4,5 +4,5 @@
 
 # You MUST use double quotes (so " and not ')
 
-__version__ = "3.0.0-develop"
-__baseline__ = "unknown"
+__version__ = "3.0.0"
+__baseline__ = "cc465c75543dfa26e3065c7dbf98354d53ad1112"
