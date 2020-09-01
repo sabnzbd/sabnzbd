420a3d385d978bb4f26541bdcb3414bdf387ebad
Safihre <safihre@sabnzbd.org>
2020-09-01 09:29:29 +0200

    Let CherryPy handle relative URL-redirection
    Need for proxies!
    https://forums.sabnzbd.org/viewtopic.php?f=2&t=24925
    https://forums.sabnzbd.org/viewtopic.php?f=11&t=24790

--- SABnzbd.py
+++ SABnzbd.py
@@ -1408,6 +1408,7 @@ def main():
 
     # Make available from both URLs
     main_page = sabnzbd.interface.MainPage()
+    cherrypy.Application.relative_urls = "server"
     cherrypy.tree.mount(main_page, "/", config=appconfig)
     cherrypy.tree.mount(main_page, sabnzbd.cfg.url_base(), config=appconfig)
 
