2f144fafb00c868635b7a0b63954d3d814aeb945
jcfp <jcfp@users.noreply.github.com>
2020-10-22 15:31:17 +0200

    only replace the first occurence of "script_"
    Use of str.replace() without a count replaces all occurences. As a result, scripts with filenames such as "my_script_for_sab.py" would be mangled when trying to set them as action on queue completion.

--- sabnzbd/__init__.py
+++ sabnzbd/__init__.py
@@ -802,7 +802,7 @@ def change_queue_complete_action(action, new=True):
     if "script_" in action:
         # all scripts are labeled script_xxx
         _action = run_script
-        _argument = action.replace("script_", "")
+        _argument = action.replace("script_", "", 1)
     elif new or cfg.queue_complete_pers.get():
         if action == "shutdown_pc":
             _action = system_shutdown
