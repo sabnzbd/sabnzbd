8d10d79ea8f4b824b1241e0e8c1ffd0b12f17144
Sander Jonkers <sander.jonkers+github@gmail.com>
2020-10-24 17:38:10 +0200

    SSDP+XML: creation of SSDP message and XML to __init__()

--- sabnzbd/utils/ssdp.py
+++ sabnzbd/utils/ssdp.py
@@ -18,6 +18,13 @@
 
 """
 sabnzbd.utils.ssdp - Support for SSDP / Simple Service Discovery Protocol plus XML to appear on Windows
+
+Method:
+1) this service send a SSDP broadcast with a description.xml in it
+2) Windows retrieves that description.xml from this service
+3) Windows presents the device in it in Windows Exporter -> Network
+
+
 """
 import logging
 import time
@@ -34,63 +41,28 @@ class SSDP(Thread):
         self.__url = url
         self.__description = description
         self.__myhostname = socket.gethostname()
-        # uuid stays the same as long as hostname and ip address stay the same:
-        self.__uuidXML = uuid.uuid3(uuid.NAMESPACE_DNS, self.__myhostname + self.__host)
-        self.__uuidSSDP = uuid.uuid3(uuid.NAMESPACE_DNS, self.__host + self.__myhostname)
+        # a steady uuid: stays the same as long as hostname and ip address stay the same:
+        self.__uuid = uuid.uuid3(uuid.NAMESPACE_DNS, self.__myhostname + self.__host)
 
-        self.__stop = False
-        super().__init__()
+        # descriptionxmlURL = self.__url + "/description.xml"
 
-    def stop(self):
-        logging.info("Stopping SSDP")
-        self.__stop = True
-
-    def run(self):
-        logging.info("Serving SSDP on %s as %s", self.__host, self.__server_name)
-        logging.info("self.__url is %s", self.__url)
-
-        descriptionxmlURL = self.__url + "/description.xml"
-
-        # the standard multicast settings for SSDP:
-        MCAST_GRP = "239.255.255.250"
-        MCAST_PORT = 1900
-        MULTICAST_TTL = 2
-
-        mySSDPbroadcast = f"""NOTIFY * HTTP/1.1
+        # Create the SSDP broadcast message
+        self.__mySSDPbroadcast = f"""NOTIFY * HTTP/1.1
 HOST: 239.255.255.250:1900
 CACHE-CONTROL: max-age=60
-LOCATION: {descriptionxmlURL}
+LOCATION: {self.__url}/description.xml
 SERVER: SABnzbd
 NT: upnp:rootdevice
-USN: uuid:{self.__uuidSSDP}::upnp:rootdevice
+USN: uuid:{self.__uuid}::upnp:rootdevice
 NTS: ssdp:alive
 OPT: "http://schemas.upnp.org/upnp/1/0/"; ns=01
 
 """
-
-        mySSDPbroadcast = mySSDPbroadcast.replace("\n", "\r\n")
-        mySSDPbroadcast = bytes(mySSDPbroadcast, "utf-8")  # convert string to bytes
-
-        while 1 and not self.__stop:
-            # Do network stuff
-            # Use self.__host, self.__url, self.__server_name to do stuff!
-
-            # Create socket, send the broadcast, and close the socket again
-            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as sock:
-                # logging.debug("Sending a SSDP multicast with size %s", len(mySSDPbroadcast))
-                sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
-                sock.sendto(mySSDPbroadcast, (MCAST_GRP, MCAST_PORT))
-            time.sleep(5)
-
-    def serve_xml(self):
-        """Returns an XML-structure based on the information being
-        served by this service, returns nothing if not running"""
-        if self.__stop:
-            return
-        # Use self.__host, self.__url, self.__server_name to do stuff!
+        self.__mySSDPbroadcast = self.__mySSDPbroadcast.replace("\n", "\r\n")
+        self.__mySSDPbroadcast = bytes(self.__mySSDPbroadcast, "utf-8")  # convert string to bytes
 
         # Create the XML info
-        myxml = f"""<?xml version="1.0" encoding="UTF-8" ?>
+        self.__myxml = f"""<?xml version="1.0" encoding="UTF-8" ?>
 <root xmlns="urn:schemas-upnp-org:device-1-0">
 <specVersion>
 <major>1</major>
@@ -104,13 +76,46 @@ OPT: "http://schemas.upnp.org/upnp/1/0/"; ns=01
 <manufacturerURL>http://www.sabnzbd.org</manufacturerURL>
 <modelDescription>SABnzbd downloader</modelDescription>
 <modelURL>http://www.sabnzbd.org</modelURL>
-<UDN>uuid:{self.__uuidXML}</UDN>
+<UDN>uuid:{self.__uuid}</UDN>
 <presentationURL>sabnzbd</presentationURL>
 </device>
 </root>"""
 
-        return myxml
-        # return f"<xml><name>{self.__server_name}</name><url>{self.__url}</url></xml>"
+        self.__stop = False
+        super().__init__()
+
+    def stop(self):
+        logging.info("Stopping SSDP")
+        self.__stop = True
+
+    def run(self):
+        logging.info("Serving SSDP on %s as %s", self.__host, self.__server_name)
+        # logging.info("self.__url is %s", self.__url)
+
+        # the standard multicast settings for SSDP:
+        MCAST_GRP = "239.255.255.250"
+        MCAST_PORT = 1900
+        MULTICAST_TTL = 2
+
+        while 1 and not self.__stop:
+            # Do network stuff
+            # Use self.__host, self.__url, self.__server_name to do stuff!
+
+            # Create socket, send the broadcast, and close the socket again
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as sock:
+                # logging.debug("Sending a SSDP multicast with size %s", len(mySSDPbroadcast))
+                sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+                sock.sendto(self.__mySSDPbroadcast, (MCAST_GRP, MCAST_PORT))
+            time.sleep(5)
+
+    def serve_xml(self):
+        """Returns an XML-structure based on the information being
+        served by this service, returns nothing if not running"""
+        if self.__stop:
+            return
+        # Use self.__host, self.__url, self.__server_name to do stuff!
+
+        return self.__myxml
 
 
 # Reserve class variable, to be started later
