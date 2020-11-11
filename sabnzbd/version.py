e8d6eebb046fe731dc18a4f88a141cf1eaab4337
Safihre <safihre@sabnzbd.org>
2020-11-11 22:04:44 +0100

    Set version to 3.1.1

--- sabnzbd/version.py
+++ sabnzbd/version.py
@@ -4,5 +4,5 @@
 
 # You MUST use double quotes (so " and not ')
 
-__version__ = "3.1.0"
-__baseline__ = "23f86e95f1f980963c4e4017276b3a3e2adfc6e2"
+__version__ = "3.1.1"
+__baseline__ = "99b5a00c12c1d8e17bb3e4a9a98339f59152c842"
