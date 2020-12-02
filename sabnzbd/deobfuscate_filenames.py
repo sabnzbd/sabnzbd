4065142253d516ea29081c2cc5b7cc2754b23e97
Sander Jonkers <sander.jonkers+github@gmail.com>
2020-12-02 18:39:02 +0100

    obfuscated: recognize fixed pattern abc.xyz as obfuscated

--- sabnzbd/deobfuscate_filenames.py
+++ sabnzbd/deobfuscate_filenames.py
@@ -80,10 +80,18 @@ def is_probably_obfuscated(myinputfilename):
     path, filename = os.path.split(myinputfilename)
     filebasename, fileextension = os.path.splitext(filename)
 
+    # First fixed patterns that we know of:
+
     # ...blabla.H.264/b082fa0beaa644d3aa01045d5b8d0b36.mkv is certainly obfuscated
     if re.findall("^[a-f0-9]{32}$", filebasename):
         logging.debug("Obfuscated: 32 hex digit")
         # exactly 32 hex digits, so:
+        return True
+
+    # /some/thing/abc.xyz.a4c567edbcbf27.BLA is certainly obfuscated
+    if re.findall("^abc\.xyz\.", filebasename):
+        logging.debug("Obfuscated: starts with 'abc.xyz.'")
+        # ... which we consider as obfuscated:
         return True
 
     # these are signals for the obfuscation versus non-obfuscation
