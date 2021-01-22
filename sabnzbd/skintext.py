8ee9bf142f3f46e59ccc441c36faa746f604306d
puzzledsab <kokkelianedi@gmail.com>
2021-01-22 12:17:02 +0100

    Give hints about intended usage in explain text

--- sabnzbd/skintext.py
+++ sabnzbd/skintext.py
@@ -625,9 +625,13 @@ SKIN_TEXT = {
     "srv-timeout": TT("Timeout"),  #: Server timeout
     "srv-connections": TT("Connections"),  #: Server: amount of connections
     "srv-expire_date": TT("Account expire date"),
-    "srv-explain-expire_date": TT("Give warning on this date. Format: YYYY-MM-DD, e.g. 2022-01-31"),
+    "srv-explain-expire_date": TT(
+        "Give warning on this date. Set a day or two before expiry to get an early warning. Format: YYYY-MM-DD"
+    ),
     "srv-quota_left": TT("Quota left on account"),
-    "srv-explain-quota_left": TT("Give warning when this much data has been used, 0 for no warning (K/M/G)."),
+    "srv-explain-quota_left": TT(
+        "Set a smaller amount than the remaining quota to get an early warning. 0 for no warning (K/M/G)."
+    ),
     "srv-retention": TT("Retention time"),  #: Server's retention time in days
     "srv-ssl": TT("SSL"),  #: Server SSL tickbox
     "explain-ssl": TT("Secure connection to server"),  #: Server SSL tickbox
