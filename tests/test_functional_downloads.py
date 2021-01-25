fc1aa2db8360e05de7651df2c45176ba3dd0ed07
Safihre <safihre@sabnzbd.org>
2021-01-25 09:27:30 +0100

    Mark test_download_unicode_made_on_windows as xfail on all platforms

--- tests/test_functional_downloads.py
+++ tests/test_functional_downloads.py
@@ -129,7 +129,7 @@ class TestDownloadFlow(SABnzbdBaseTest):
     def test_download_passworded(self):
         self.download_nzb("test_passworded{{secret}}", "testfile.bin")
 
-    @pytest.mark.xfail(not sys.platform.startswith("win"), reason="Probably #1633")
+    @pytest.mark.xfail(reason="Probably #1633")
     def test_download_unicode_made_on_windows(self):
         self.download_nzb("test_win_unicode", "frènch_german_demö.bin")
 
