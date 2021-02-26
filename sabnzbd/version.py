16618b3af240685d79ad83387d8d3df817dbb1af
Safihre <safihre@sabnzbd.org>
2021-02-26 10:30:00 +0100

    Set version to 3.2.0

--- sabnzbd/version.py
+++ sabnzbd/version.py
@@ -1,8 +1,9 @@
 # This file will be patched by setup.py
 # The __version__ should be set to the branch name
+# Leave __baseline__ set to unknown to enable setting commit-hash
 # (e.g. "develop" or "1.2.x")
 
 # You MUST use double quotes (so " and not ')
 
-__version__ = "3.1.1"
-__baseline__ = "99b5a00c12c1d8e17bb3e4a9a98339f59152c842"
+__version__ = "3.2.0"
+__baseline__ = "7be9281431315ecfff806bb1d47106afbeca2e89"
