163041cf6c7b666f4967f35b897fd3a7c86b5edd
Sander Jonkers <sander.jonkers+github@gmail.com>
2021-12-21 21:59:32 +0100

    if too little disk space reported by 7z unzipping, report so in GUI

--- sabnzbd/newsunpack.py
+++ sabnzbd/newsunpack.py
@@ -1053,6 +1053,9 @@ def seven_extract_core(sevenset, extensions, extraction_path, one_folder, delete
         # We can output a more general error
         ret = 1
         msg = T('ERROR: CRC failed in "%s"') % setname_from_path(sevenset)
+    elif ret == 2 and (("Disk full." in output) or ("No space left on device" in output)):
+        ret = 1 # to avoid ret = 2, causing error about password
+        msg = T('ERROR: Not enough disk space for unzipping "%s"') % setname_from_path(sevenset)
     else:
         # Default message
         msg = T("Could not unpack %s") % setname_from_path(sevenset)
