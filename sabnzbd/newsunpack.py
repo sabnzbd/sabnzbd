e805af4c3358ff5c839cebc5f7a24f03029b6d4e
Sander Jonkers <sander.jonkers+github@gmail.com>
2022-01-01 14:03:56 +0100

    setname_from_path(sevenset)

--- sabnzbd/newsunpack.py
+++ sabnzbd/newsunpack.py
@@ -1090,7 +1090,11 @@ def seven_extract_core(sevenset, extensions, extraction_path, one_folder, delete
     # 7z cases we don't handle
     if ret > 2:
         # 7 = Bad command line parameters, 8 = Not enough memory for operation, 255 = User stopped the process
-        msg = T("Unpacking failed, %s") % sevenset + ". Return code 7z/7za: %s. " % str(ret) + T("see logfile")
+        msg = (
+            T("Unpacking failed, %s") % setname_from_path(sevenset)
+            + ". Return code 7z/7za: %s. " % str(ret)
+            + T("see logfile")
+        )
 
     # Always return an error message, even when return code is 0
     return ret, new_files, msg
