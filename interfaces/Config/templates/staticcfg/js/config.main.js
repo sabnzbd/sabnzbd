bdd03c06b4ea936329ee66a333804cd2f160cbf7
SKBotNL <68018116+SKBotNL@users.noreply.github.com>
2022-01-26 15:40:25 +0100

    Add shortcuts to the config

---
+++ interfaces/Config/templates/staticcfg/js/config.main.js
@@ -0,0 +1,44 @@
+self.keyboardShortcuts = ko.observable(true).extend({ persist: 'keyboardShortcuts' });
+
+document.onkeydown = function(e) {
+    if(self.keyboardShortcuts()) {
+        // Ignore if the user used a combination
+        if(e.altKey || e.metaKey || e.ctrlKey) return;
+
+        // Do not act if the user is typing something
+        if($('input:focus, textarea:focus').length === 0) {
+            configTabs = [
+                '/sabnzbd/config/general/',
+                '/sabnzbd/config/folders/',
+                '/sabnzbd/config/server/',
+                '/sabnzbd/config/categories/',
+                '/sabnzbd/config/switches/',
+                '/sabnzbd/config/sorting/',
+                '/sabnzbd/config/notify/',
+                '/sabnzbd/config/scheduling/',
+                '/sabnzbd/config/rss/',
+                '/sabnzbd/config/special/'
+            ]
+            if (e.code === 'ArrowRight') {
+                nextTabLocation = configTabs.indexOf(window.location.pathname) + 1;
+                if (nextTabLocation >= configTabs.length) {
+                    nextTabLocation = 0;
+                }
+                console.log(nextTabLocation);
+                window.location.pathname = configTabs[nextTabLocation];
+            }
+            if (e.code === 'ArrowLeft') {
+                nextTabLocation = configTabs.indexOf(window.location.pathname) - 1;
+                window.location.pathname = configTabs.slice(nextTabLocation)[0];
+            }
+            if (e.code === 'KeyS') {
+                $('.search-dropdown').dropdown('toggle');
+                window.event.preventDefault();
+            }
+            if (e.code === 'KeyM') {
+                window.location.href = `${window.location.host}/sabnzbd/`;
+            }
+        }
+
+    }
+}
