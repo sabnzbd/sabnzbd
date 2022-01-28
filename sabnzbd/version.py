a756eea25aa2ba879962a2de78d9f4fbdf30723a
Safihre <safihre@sabnzbd.org>
2022-01-28 11:47:15 +0100

    Set version to 3.5.0

--- sabnzbd/version.py
+++ sabnzbd/version.py
@@ -5,5 +5,5 @@
 
 # You MUST use double quotes (so " and not ')
 
-__version__ = "3.4.2"
-__baseline__ = "d8ab19087d541cb3c6bfe123d8f8e2619946995a"
+__version__ = "3.5.0"
+__baseline__ = "e586ead024510309058ce5dfb6b9400fe86ec716"
