b323fe72b25a9032757b28a4ddad861d73a0e879
puzzledsab <50911832+puzzledsab@users.noreply.github.com>
2022-12-03 19:47:45 +0100

    Do chunk size calculations in readsock

--- sabnzbd/readsock.py
+++ sabnzbd/readsock.py
@@ -6,9 +6,13 @@ from typing import List
 def readsock(sock: socket.socket):
     data: List[bytes] = []
     readcount: int = 0
+    totalsize: int = 0
+    chunksize: int = 0
     endtime: float = time.time() + 0.00011
     while 1:
         data.append(sock.recv(16384))
+        chunksize = len(data[-1])
+        totalsize += chunksize
         readcount += 1
-        if readcount >= 16 or time.time() > endtime or len(data[-1]) < 16384:
-            return data
+        if readcount >= 16 or chunksize < 16384 or time.time() > endtime:
+            return data, chunksize
