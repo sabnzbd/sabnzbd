db046807265925899c5faabfe9886243580ab191
puzzledsab <50911832+puzzledsab@users.noreply.github.com>
2022-12-05 19:58:00 +0100

    Revert "Refactor article.get_article (#2344)"
    This reverts commit 63018439c8cf3b8c5d476a7a411cc96c5da00b25.

--- tests/test_nzbstuff.py
+++ tests/test_nzbstuff.py
@@ -50,52 +50,6 @@ class TestNZO:
         # TODO: More checks!
 
 
-class Server:
-    def __init__(self, host, priority, active):
-        self.host = host
-        self.priority = priority
-        self.active = active
-
-
-class TestArticle:
-    def test_get_article(self):
-        article_id = "test@host" + os.urandom(8).hex() + ".sab"
-        article = nzbstuff.Article(article_id, randint(4321, 54321), None)
-        servers = []
-        servers.append(Server("testserver1", 10, True))
-        servers.append(Server("testserver2", 20, True))
-        servers.append(Server("testserver3", 30, True))
-
-        # Test fetching top priority server
-        server = servers[0]
-        assert article.get_article(server, servers) == article
-        assert article.fetcher_priority == 10
-        assert article.fetcher == server
-        assert article.get_article(server, servers) == None
-        article.fetcher = None
-        article.add_to_try_list(server)
-        assert article.get_article(server, servers) == None
-
-        # Test fetching when there is a higher priority server available
-        server = servers[2]
-        assert article.fetcher_priority == 10
-        assert article.get_article(server, servers) == None
-        assert article.fetcher_priority == 20
-
-        # Server should be used even if article.fetcher_priority is a higher number than server.priority
-        article.fetcher_priority = 30
-        server = servers[1]
-        assert article.get_article(server, servers) == article
-
-        # Inactive servers in servers list should be ignored
-        article.fetcher = None
-        article.fetcher_priority = 0
-        servers[1].active = False
-        server = servers[2]
-        assert article.get_article(server, servers) == article
-        assert article.tries == 3
-
-
 class TestNZBStuffHelpers:
     @pytest.mark.parametrize(
         "argument, name, password",
