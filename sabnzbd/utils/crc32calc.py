684cfb200c719742f99d20a1c0c7f603c8b072c5
puzzledsab <50911832+puzzledsab@users.noreply.github.com>
2023-01-07 22:45:49 +0100

    Update credits in crc32.py, use crc32 in test_par2file.py

--- sabnzbd/utils/crc32calc.py
+++ sabnzbd/utils/crc32calc.py
@@ -1,25 +1,11 @@
 #!/usr/bin/python3 -OO
-# Copyright 2007-2022 The SABnzbd-Team <team@sabnzbd.org>
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 """
-sabnzbd.crc32 - Functions for doing CRC32 calculations
+Functions for doing CRC32 calculations
+Written by animetoshi
+https://github.com/sabnzbd/sabnzbd/issues/2396
 """
 
-# CRC32 stuff from animetoshi
 CRC32_POLYNOMIAL = 0xEDB88320
 CRC32_POWER_TABLE = [  # pre-computed 2**(2**n)
     0x40000000,
