4bf0be6b9a81457957381c752de38de367a44d45
puzzledsab <50911832+puzzledsab@users.noreply.github.com>
2023-01-29 15:25:24 +0100

    Not easy to keep black happy

--- sabnzbd/downloader.py
+++ sabnzbd/downloader.py
@@ -852,11 +852,7 @@ class Downloader(Thread):
             # Reset connection for new activity
             nw.soft_reset()
             # Request a new article immediately if possible
-            if (
-                nw.connected
-                and server.active
-                and not (self.paused or self.shutdown or self.paused_for_postproc)
-            ):
+            if nw.connected and server.active and not (self.paused or self.shutdown or self.paused_for_postproc):
                 nw.article = server.get_article()
                 if nw.article:
                     self.__request_article(nw)
