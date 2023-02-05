f5bff8fe7c119e957be8d39cee3cdeeddda6901b
Safihre <safihre@sabnzbd.org>
2023-02-05 22:07:42 +0100

    Update copyright year to 2023

--- sabnzbd/skintext.py
+++ sabnzbd/skintext.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3 -OO
 # -*- coding: UTF-8 -*-
-# Copyright 2007-2022 The SABnzbd-Team <team@sabnzbd.org>
+# Copyright 2007-2023 The SABnzbd-Team <team@sabnzbd.org>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
