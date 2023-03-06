09f58613e57ce026f069f4dac1bb92c42d13d948
Safihre <safihre@sabnzbd.org>
2023-03-06 22:00:08 +0100

    Some styling things

--- sabnzbd/skintext.py
+++ sabnzbd/skintext.py
@@ -762,7 +762,7 @@ SKIN_TEXT = {
         "<br>Sorters are tried in order of appearance, and can be reordered by dragging and dropping. The first active sorter that meets both selection criteria (affected category and job type) is applied to a job, provided at least one file meets the minumum filesize requirement."
     ),
     "add-sorter": TT("Add Sorter"),
-    "new-sorter": TT("New Sorter"),
+    "remove-sorter": TT("Remove Sorter"),
     "sort-test-data": TT("Test Data"),
     # Config->Special
     "explain-special": TT(
