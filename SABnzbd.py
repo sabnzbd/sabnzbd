7cbbff727fa0da4c37be6a565866a2776a2ffea3
Safihre <safihre@sabnzbd.org>
2023-07-21 11:33:51 +0200

    Remove (almost) all references to unused team@sabnzbd.org

--- SABnzbd.py
+++ SABnzbd.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3 -OO
-# Copyright 2007-2023 The SABnzbd-Team <team@sabnzbd.org>
+# Copyright 2007-2023 The SABnzbd-Team (sabnzbd.org)
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -244,7 +244,7 @@ def print_version():
             """
 %s-%s
 
-Copyright (C) 2007-2023 The SABnzbd-Team <team@sabnzbd.org>
+Copyright (C) 2007-2023 The SABnzbd-Team (sabnzbd.org)
 SABnzbd comes with ABSOLUTELY NO WARRANTY.
 This is free software, and you are welcome to redistribute it
 under certain conditions. It is licensed under the
--- builder/win/NSIS_Installer.nsi
+++ builder/win/NSIS_Installer.nsi
@@ -1,6 +1,6 @@
 ; -*- coding: utf-8 -*-
 ;
-; Copyright 2008-2015 The SABnzbd-Team <team@sabnzbd.org>
+; Copyright 2008-2015 The SABnzbd-Team (sabnzbd.org)
 ;
 ; This program is free software; you can redistribute it and/or
 ; modify it under the terms of the GNU General Public License
