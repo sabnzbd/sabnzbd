e4d16266a10c9e01df81c1b769493a12bf7e3ff3
Safihre <safihre@sabnzbd.org>
2023-10-22 13:02:36 +0200

    Process feedback

--- sabnzbd/happyeyeballs.py
+++ sabnzbd/happyeyeballs.py
@@ -23,7 +23,7 @@ sabnzbd.happyeyeballs - Python implementation of RFC 6555 / Happy Eyeballs: find
 # See https://tools.ietf.org/html/rfc6555
 # Method: Start parallel sessions using threads, and only wait for the quickest successful socket connect
 # See https://tools.ietf.org/html/rfc6555#section-4.1
-# We do not implement caching, as this is handled by the SABnzbd calling code
+# We do not implement caching, as the lookup result is stored in the Server object
 
 import socket
 import threading
@@ -34,6 +34,7 @@ from typing import NamedTuple, Tuple, Union, Optional
 
 from sabnzbd import cfg as cfg
 
+# We always prefer IPv6 connections
 IP4_DELAY = 0.1
 
 
@@ -47,15 +48,15 @@ class AddrInfo(NamedTuple):
 
 
 # Called by each thread
-def do_socket_connect(result_queue: queue.Queue, addrinfo: AddrInfo):
+def do_socket_connect(result_queue: queue.Queue, addrinfo: AddrInfo, ipv4_delay: int):
     """Connect to the ip, and put the result into the queue"""
     try:
         s = socket.socket(addrinfo.family, addrinfo.type)
         s.settimeout(3)
 
         # Delay IPv4 connects in case we need it
-        if addrinfo.family == socket.AddressFamily.AF_INET:
-            time.sleep(IP4_DELAY)
+        if ipv4_delay and addrinfo.family == socket.AddressFamily.AF_INET:
+            time.sleep(ipv4_delay)
 
         try:
             s.connect(addrinfo.sockaddr)
@@ -68,40 +69,41 @@ def do_socket_connect(result_queue: queue.Queue, addrinfo: AddrInfo):
 
 
 def happyeyeballs(host: str, port: int) -> Optional[AddrInfo]:
-    """Return the fastest result of getaddrinfo()
-    based on RFC 6555 / Happy Eyeballs,
-    including IPv6 addresses if desired"""
+    """Return the fastest result of getaddrinfo() based on RFC 6555 / Happy Eyeballs,
+    including IPv6 addresses if desired. Returns None in case no addresses were returned
+    or if no connection could be made to any of the addresses"""
     try:
         # Time how long it took us
         start = time.time()
 
-        # Get address information, by default IPV4 or IPV6
+        # Get address information, by default both IPV4 and IPV6
         family = socket.AF_UNSPEC
         if not cfg.ipv6_servers():
-            # Only IPv4
             family = socket.AF_INET
-        all_addrinfo = socket.getaddrinfo(host, port, family, socket.SOCK_STREAM, flags=socket.AI_CANONNAME)
+
+        all_addrinfo = []
+        ipv4_delay = 0
+        for addrinfo in socket.getaddrinfo(host, port, family, socket.SOCK_STREAM, flags=socket.AI_CANONNAME):
+            # Convert to AddrInfo
+            all_addrinfo.append(addrinfo := AddrInfo(*addrinfo))
+            # We only want delay for IPv4 in case we got any IPv6
+            if addrinfo.family == socket.AddressFamily.AF_INET6:
+                ipv4_delay = IP4_DELAY
         logging.debug("Available addresses for %s (port=%d): %d", host, port, len(all_addrinfo))
 
-        # Convert to AddrInfo
-        all_addrinfo = [AddrInfo(*addrinfo) for addrinfo in all_addrinfo]
+        # Fill queue used for threads that will return the results
+        # Even if there is just 1 result, we still check if we can connect
+        result_queue: queue.Queue[Tuple[AddrInfo, bool]] = queue.Queue()
+        for addrinfo in all_addrinfo:
+            threading.Thread(target=do_socket_connect, args=(result_queue, addrinfo, ipv4_delay), daemon=True).start()
 
-        # Skip speed comparison in case we only have 1 result
-        if len(all_addrinfo) > 1:
-            # Fill queue used for threads giving back the results
-            result_queue: queue.Queue[Tuple[AddrInfo, bool]] = queue.Queue()
-            for addrinfo in all_addrinfo:
-                threading.Thread(target=do_socket_connect, args=(result_queue, addrinfo), daemon=True).start()
-
-            # start reading from the Queue for message from the threads:
-            result = None
-            for _ in range(len(all_addrinfo)):
-                connect_result = result_queue.get()
-                if connect_result[1]:
-                    result = connect_result[0]
-                    break
-        else:
-            result = all_addrinfo[0]
+        # start reading from the Queue for message from the threads:
+        result = None
+        for _ in range(len(all_addrinfo)):
+            connect_result = result_queue.get()
+            if connect_result[1]:
+                result = connect_result[0]
+                break
 
         logging.info("Quickest IP address for %s (port=%d): %s", host, port, result.sockaddr[0])
         logging.debug("Happy Eyeballs lookup and port connect took: %d ms", int(1000 * (time.time() - start)))
