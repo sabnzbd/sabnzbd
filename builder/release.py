9f6a9f991222efccc87b45a701086c95629c67b6
thezoggy <thezoggy@gmail.com>
2024-01-01 18:40:26 -0600

    normalize and update copyright year

--- builder/release.py
+++ builder/release.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3 -OO
-# Copyright 2008-2017 The SABnzbd-Team (sabnzbd.org)
+# Copyright 2008-2024 by The SABnzbd-Team (sabnzbd.org)
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
