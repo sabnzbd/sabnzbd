0f557dbc9f95d1c9b1d524f416cf3645c6495e9b
sander <sander.jonkers+github@gmail.com>
2024-07-15 07:00:25 +0200

    make black happy ... hopefully ... linelength 120

--- sabnzbd/internetspeed.py
+++ sabnzbd/internetspeed.py
@@ -38,17 +38,13 @@ TEST_FILE = "/tests/internetspeed/100MB.bin"
 TEST_FILE_SIZE = 100 * 1024 * 1024
 TEST_REQUEST = f"GET {TEST_FILE} HTTP/1.1\nHost: {TEST_HOSTNAME}\nUser-Agent: SABnzbd/{sabnzbd.__version__}\n\n"
 SOCKET_TIMEOUT = 3
-BUFFER_SIZE = (
-    5 * 1024 * 1024
-)  # Each connection will allocate its own buffer, so mind the memory usage!
+BUFFER_SIZE = 5 * 1024 * 1024  # Each connection will allocate its own buffer, so mind the memory usage!
 
 NR_CONNECTIONS = 5
 TIME_LIMIT = 3
 
 
-def internetspeed_worker(
-    secure_sock: ssl.SSLSocket, socket_speed: Dict[ssl.SSLSocket, float]
-):
+def internetspeed_worker(secure_sock: ssl.SSLSocket, socket_speed: Dict[ssl.SSLSocket, float]):
     """Worker to perform the requests in parallel"""
     secure_sock.sendall(TEST_REQUEST.encode())
     empty_buffer = memoryview(sabctools.bytearray_malloc(BUFFER_SIZE))
@@ -60,9 +56,7 @@ def internetspeed_worker(
     while diff_time < TIME_LIMIT:
         if data_received < TEST_FILE_SIZE:
             try:
-                if new_bytes := sabctools.unlocked_ssl_recv_into(
-                    secure_sock, empty_buffer
-                ):
+                if new_bytes := sabctools.unlocked_ssl_recv_into(secure_sock, empty_buffer):
                     # Update the speed after every loop
                     diff_time = time.perf_counter() - start_time
                     data_received += new_bytes
@@ -81,9 +75,7 @@ def internetspeed_worker(
         pass
 
 
-def internetspeed_interal(
-    test_time_limit: int = TIME_LIMIT, family: int = socket.AF_UNSPEC
-) -> float:
+def internetspeed_interal(test_time_limit: int = TIME_LIMIT, family: int = socket.AF_UNSPEC) -> float:
     """Measure internet speed from a test-download using our optimized SSL-code"""
 
     context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
@@ -92,7 +84,7 @@ def internetspeed_interal(
     try:
         addrinfo = happyeyeballs(TEST_HOSTNAME, TEST_PORT, SOCKET_TIMEOUT, family)
         if not addrinfo:
-            # no addrinfo from happyeyeballs, so no connection possible
+            # no addrinfo from happyeyeballs, so no connection was possible
             return 0.0  # no speed at all
         for _ in range(NR_CONNECTIONS):
             sock = socket.socket(addrinfo.family, addrinfo.type)
