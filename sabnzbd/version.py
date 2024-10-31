3d74c19f41379b0bad40546faf045cb67e6d5b4d
Safihre <safihre@sabnzbd.org>
2024-10-31 21:22:40 +0100

    Update text files for 4.4.0Beta1

--- sabnzbd/version.py
+++ sabnzbd/version.py
@@ -6,5 +6,5 @@
 # You MUST use double quotes (so " and not ')
 # Do not forget to update the appdata file for every major release!
 
-__version__ = "4.4.0Alpha2"
+__version__ = "4.4.0Beta1"
 __baseline__ = "unknown"
