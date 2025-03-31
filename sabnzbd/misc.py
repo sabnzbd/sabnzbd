7a4978ba0935b3843565bd34978e1936fc2e40d5
mreid-tt <943378+mreid-tt@users.noreply.github.com>
2025-03-31 17:04:58 -0400

    Simplify solution to suppress stderr

--- sabnzbd/misc.py
+++ sabnzbd/misc.py
@@ -818,11 +818,9 @@ def get_platform_description() -> str:
         else:
             # Check for other forms of virtualization
             try:
-                # Check if the command exists by searching in the PATH
-                if run_command(["which", "systemd-detect-virt"]).strip():
-                    if virt := run_command(["systemd-detect-virt"]).strip():
-                        if virt != "none":
-                            platform_tags.append(virt)
+                if virt := run_command(["systemd-detect-virt"], stderr=subprocess.DEVNULL).strip():
+                    if virt != "none":
+                        platform_tags.append(virt)
             except:
                 pass
 
