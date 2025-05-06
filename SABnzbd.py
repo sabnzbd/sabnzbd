0220d886c59a72db3f7834d031f0433735a85df8
jcfp <jcfp@users.noreply.github.com>
2025-05-06 07:07:43 +0000

    prevent cherrypy from stumbling over invalid cookies (#3082)
    * prevent cherrypy from stumbling over invalid cookies
    
    * group workarounds for flaws in external mods

--- SABnzbd.py
+++ SABnzbd.py
@@ -39,6 +39,7 @@ import time
 import re
 import gc
 import threading
+import http.cookies
 from typing import List, Dict, Any
 
 try:
@@ -1288,6 +1289,9 @@ def main():
             "error_page.404": sabnzbd.panic.error_page_404,
         }
     )
+
+    # Monkey-patch key validation to prevent cherrypy from stumbling over invalid cookies
+    http.cookies._is_legal_key = lambda _: True
 
     # Catch shutdown errors that can break cherrypy/cheroot
     # See https://github.com/cherrypy/cheroot/issues/710
