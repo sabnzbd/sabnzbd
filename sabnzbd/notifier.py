2d8be8d89b8cd016a02782e236ed855b21423f0a
Safihre <safihre@sabnzbd.org>
2025-06-23 12:36:39 +0200

    Disable forced check for Windows verison
    See https://github.com/pywinrt/pywinrt/issues/136

--- sabnzbd/notifier.py
+++ sabnzbd/notifier.py
@@ -53,9 +53,6 @@ if sabnzbd.WINDOWS:
         shell.SetCurrentProcessExplicitAppUserModelID("SABnzbd")
         _HAVE_WINDOWS_TOASTER = True
     except Exception:
-        # This needs to work on Windows releases
-        if hasattr(sys, "frozen"):
-            raise
         # Sending toasts on non-supported platforms results in segfaults
         _HAVE_WINDOWS_TOASTER = False
 
