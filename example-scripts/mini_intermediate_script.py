8966e344e47fe0f15fe4067058c60565e0cc63e4
sanderjo <sander.jonkers+github@github.com>
2025-12-09 22:53:53 +0100

    Example intermediate scripts

---
+++ example-scripts/mini_intermediate_script.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import sys, os, glob
+from pathlib import Path
+
+"""
+Output = Priority that SABnzbd will set for the job
+-100 = Default
+-2 = Paused
+-1 = Low .... # mkv without subs
+0 = Normal ... # other filetypes
+1 = High .... # mkv with subs
+2 = Force
+"""
+
+# MAIN
+
+# get the directory name from command line arguments
+
+try:
+    # Parse the input variables for SABnzbd version >= 4.2.0
+    (scriptname, dirname) = sys.argv  # we only need dirname
+except Exception:
+    print("specificy dirname!!!")
+    sys.exit(1)  # a non-zero exit status causes SABnzbd to ignore the output of this script
+
+
+# check if dirname exists
+if not os.path.exists(dirname):
+    print(f"Directory {dirname} does not exist!")
+    sys.exit(1)
+
+
+# And this is an example: set priority based on first letter of dirname
+prio = 2  # default priority
+# set prio to 1 if first letter of dirname is A-M
+first_letter = os.path.basename(dirname)[0].upper()
+if first_letter >= "A" and first_letter <= "H":
+    prio = 1
+elif first_letter >= "P" and first_letter <= "Z":
+    prio = -1
+
+if dirname.find("AAA") != -1:
+    prio = 2  # Force
+elif dirname.find("ZZZ") != -1:
+    prio = -2  # Paused
+
+# same output as SABnzbd pre-queue script:
+
+print("1")  # Accept the job
+print()  # print(f"my result for dirname {dirname}") # "job name" ...
+print()
+print()
+print()
+print(prio)  # Priority
+print()
+
+# 0 means OK
+sys.exit(0)
---
+++ example-scripts/mkv_intermediate-script.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import sys, os, glob
+from pathlib import Path
+
+"""
+Priority
+-100 = Default
+-2 = Paused
+-1 = Low .... # mkv without subs
+0 = Normal ... # other filetypes
+1 = High .... # mkv with subs
+2 = Force
+"""
+
+
+def get_biggest_file_in_dir(directory: str):
+    """Return the biggest file in a directory"""
+    # pattern = os.path.join(directory, "**/*")
+    # all_files = [path for path in glob.glob(pattern, recursive=True) if os.path.isfile(path)]
+
+    # glob has problems with special characters like "[" and "]", so use pathlib
+    target_dir = Path(directory)
+    all_files = [f for f in target_dir.iterdir() if f.is_file()]
+    if not all_files:
+        return None, 0
+    all_files = sorted(all_files, key=os.path.getsize)[::-1]  # reversed, so big to small. Format [start:stop:step]
+    return all_files[0], os.path.getsize(all_files[0])
+
+
+try:
+    # Parse the input variables for SABnzbd version >= 4.2.0
+    (scriptname, dirname) = sys.argv  # we only need dirname
+except Exception:
+    print("specificy dirname!!!")
+    sys.exit(1)  # a non-zero exit status causes SABnzbd to ignore the output of this script
+
+
+def mkv_has_subs(mkv_file: str) -> bool:
+    """Check if an MKV file has subtitle tracks.
+    Ugly hack ahead: Just count the nubmer of occurences of "S_TEXT/UTF8" in the first 10_000 bytes of the file.
+    """
+
+    # read first 10_000 bytes of the file, and count the number of occurences of "S_TEXT/UTF8" or "S_TEXT/ASS"
+    try:
+        with open(mkv_file, "rb") as f:
+            data = f.read(10_000)
+            count_subs = data.count(b"S_TEXT/UTF8") + data.count(b"S_TEXT/ASS")
+            if count_subs > 0:
+                return True
+            else:
+                return False
+    except Exception as e:
+        print(f"Error reading MKV file {mkv_file}: {e}")
+        return False
+
+
+# MAIN
+
+prio = 0  # default priority
+
+# check if dirname exists
+if not os.path.exists(dirname):
+    print(f"Directory {dirname} does not exist!")
+    sys.exit(1)
+
+biggest_file, _ = get_biggest_file_in_dir(dirname)
+
+if biggest_file is None:
+    print(f"No files found in directory {dirname}")
+    sys.exit(0)
+
+# find extension of biggest file
+
+_, ext = os.path.splitext(biggest_file)
+ext = ext.lower()
+if ext == ".mkv":
+    if mkv_has_subs(biggest_file):
+        prio = 2  # higher priority
+    else:
+        prio = -1  # lower priority
+
+
+# same output as SABnzbd pre-queue script:
+
+print("1")  # Accept the job
+print(f"my result for dirname {dirname} is ... {biggest_file}")  # "job name" ...
+print()
+print()
+print()
+print(prio)  # Priority
+print()
+
+# 0 means OK
+sys.exit(0)
