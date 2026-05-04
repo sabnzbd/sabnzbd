98dc18388139ef9487e2b67479fefcef62a2d219
Safihre <safihre@sabnzbd.org>
2026-05-04 16:43:42 +0200

    Add test for 3.0.0 legacy queue format restoration
    Moved sys.modules call higher, so it is always applied

--- tests/test_nzbqueue.py
+++ tests/test_nzbqueue.py
@@ -19,6 +19,8 @@
 tests.test_nzbqueue - Testing functions in nzbqueue.py
 """
 
+import shutil
+from pathlib import Path
 from types import SimpleNamespace
 
 from sabnzbd.downloader import Server
@@ -109,3 +111,51 @@ class TestNzbQueue:
 
         # Try list restored
         assert sabnzbd.Downloader.servers[0] in list(joba.files[0].articles)[0].try_list
+
+    @pytest.mark.skipif(not sabnzbd.WINDOWS, reason="Legacy 3.0.0 queue fixture contains Windows-specific paths")
+    def test_restore_legacy_queue_format_3_0_0(self, tmp_path, monkeypatch):
+        fixture_path = Path(SAB_DATA_DIR) / "test_3_0_0_queue_format"
+        shutil.copytree(fixture_path, tmp_path, dirs_exist_ok=True)
+
+        nzbqueue = NzbQueue()
+        nzbqueue.read_queue(repair=0)
+
+        assert nzbqueue.actives() == 1
+        nzo = nzbqueue.get_nzo("SABnzbd_nzo_7ormqfeg")
+
+        # Check a number of basis statistics and parameters
+        assert nzo.nzo_id == "SABnzbd_nzo_7ormqfeg"
+        assert nzo.work_name == "test_download_100MB"
+        assert nzo.filename == "test_download_100MB.nzb"
+        assert nzo.cat == "*"
+        assert nzo.priority == 1
+        assert nzo.pp == 3
+
+        assert len(nzo.files_table) == 13
+        assert len(nzo.files) == 4
+        assert len(nzo.finished_files) == 1
+        assert len(nzo.extrapars["par-files"]) == 8
+        assert len(nzo.saved_articles) == 22
+
+        assert nzo.bytes == 114967316
+        assert nzo.bytes_downloaded == 18532805
+        assert nzo.bytes_missing == 0
+        assert nzo.bytes_par2 == 42560
+        assert nzo.bytes_tried == 30077349
+        assert nzo.remaining == 84889967
+
+        # Validate we can also load legacy NZF pickles from the restored NZO
+        for nzf in nzo.files:
+            nzf.finish_import()
+
+        # Validate from specifc nzf some attributes
+        cnzf = nzo.get_nzf_by_id("SABnzbd_nzf_799tdim0")
+        assert cnzf.nzo == nzo
+        assert cnzf.filename == "rar-files.part1.rar"
+        assert cnzf.md5of16k == b"e\xc4^\x93\xc4\x92\xf9s\x057Oe!\x9dA\xe7"
+        assert len(cnzf.articles) == 51
+        assert len(cnzf.decodetable) == 73
+
+        assert cnzf.bytes == 27049565
+        assert cnzf.bytes_left == 10777869
+        assert cnzf.crc32 is None
