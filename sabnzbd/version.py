45873d4cbb362661e5f78b36891e19ab6961de91
Safihre <safihre@sabnzbd.org>
2026-05-14 20:16:11 +0200

    Update text files for 5.0.2

--- sabnzbd/version.py
+++ sabnzbd/version.py
@@ -6,5 +6,5 @@
 # You MUST use double quotes (so " and not ')
 # Do not forget to update the appdata file for every major release!
 
-__version__ = "5.0.2RC1"
+__version__ = "5.0.2"
 __baseline__ = "unknown"
