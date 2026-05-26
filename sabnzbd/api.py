89017b0d40900c40ec4d37f22338a42498721c7f
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2026-05-26 21:48:04 +0100

    Less crazy remote label replacements

--- sabnzbd/api.py
+++ sabnzbd/api.py
@@ -700,29 +700,12 @@ LOG_INI_HIDE_RE = re.compile(
 )
 LOG_NNTP_AUTH_RE = re.compile(rb"(authinfo (?:user|pass)) [^\\'\'\r\n]+", re.I)
 LOG_HASH_RE = re.compile(rb"([a-zA-Z\d]{25})", re.I)
-
-# https://gist.github.com/dfee/6ed3a4b05cfe7a6faf40a2102408d5d8
-# fmt: off
-IPV4SEG  = r'(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])'
-IPV4ADDR = r'(?:(?:' + IPV4SEG + r'\.){3,3}' + IPV4SEG + r')'
-IPV6SEG  = r'(?:(?:[0-9a-fA-F]){1,4})'
-IPV6GROUPS = (
-    r'(?:' + IPV6SEG + r':){7,7}' + IPV6SEG,                  # 1:2:3:4:5:6:7:8
-    r'(?:' + IPV6SEG + r':){1,7}:',                           # 1::                                 1:2:3:4:5:6:7::
-    r'(?:' + IPV6SEG + r':){1,6}:' + IPV6SEG,                 # 1::8               1:2:3:4:5:6::8   1:2:3:4:5:6::8
-    r'(?:' + IPV6SEG + r':){1,5}(?::' + IPV6SEG + r'){1,2}',  # 1::7:8             1:2:3:4:5::7:8   1:2:3:4:5::8
-    r'(?:' + IPV6SEG + r':){1,4}(?::' + IPV6SEG + r'){1,3}',  # 1::6:7:8           1:2:3:4::6:7:8   1:2:3:4::8
-    r'(?:' + IPV6SEG + r':){1,3}(?::' + IPV6SEG + r'){1,4}',  # 1::5:6:7:8         1:2:3::5:6:7:8   1:2:3::8
-    r'(?:' + IPV6SEG + r':){1,2}(?::' + IPV6SEG + r'){1,5}',  # 1::4:5:6:7:8       1:2::4:5:6:7:8   1:2::8
-    IPV6SEG + r':(?:(?::' + IPV6SEG + r'){1,6})',             # 1::3:4:5:6:7:8     1::3:4:5:6:7:8   1::8
-    r':(?:(?::' + IPV6SEG + r'){1,7}|:)',                     # ::2:3:4:5:6:7:8    ::2:3:4:5:6:7:8  ::8       ::
-    r'fe80:(?::' + IPV6SEG + r'){0,4}%[0-9a-zA-Z]{1,}',       # fe80::7:8%eth0     fe80::7:8%1  (link-local IPv6 addresses with zone index)
-    r'::(?:ffff(?::0{1,4}){0,1}:){0,1}[^\s:]' + IPV4ADDR,     # ::255.255.255.255  ::ffff:255.255.255.255  ::ffff:0:255.255.255.255 (IPv4-mapped IPv6 addresses and IPv4-translated addresses)
-    r'(?:' + IPV6SEG + r':){1,4}:[^\s:]' + IPV4ADDR,          # 2001:db8:3:4::192.0.2.33  64:ff9b::192.0.2.33 (IPv4-Embedded IPv6 Address)
+LOG_REMOTE_LABEL_RE = re.compile(
+    rb"(?P<prefix>^.*?]\s*.*?)"
+    rb"(?P<ip>\S+)"
+    rb"(?:\s+\(X-Forwarded-For:\s*(?P<xff>[^)]+)\))?"
+    rb"\s+\[(?P<ua>[^]]+)]"
 )
-# fmt: on
-IPV4_RE = re.compile(IPV4ADDR.encode())
-IPV6_RE = re.compile("|".join([f"(?:{g})" for g in IPV6GROUPS[::-1]]).encode())  # Reverse rows for greedy match
 
 
 def _api_showlog(name: str, kwargs: ApiParams) -> Generator[bytes, Any, None]:
@@ -749,17 +732,18 @@ def _api_showlog(name: str, kwargs: ApiParams) -> Generator[bytes, Any, None]:
     except Exception:
         pass
 
+    def remote_label_replacement(m: re.Match[bytes]) -> bytes:
+        if m.group("xff"):
+            return b"%s<REMOVED> (X-Forwarded-For: <REMOVED>) [%s]" % (m.group("prefix"), m.group("ua"))
+        return b"%s<REMOVED> [%s]" % (m.group("prefix"), m.group("ua"))
+
     def sanitize_line(line: bytes) -> bytes:
         """Apply regex substitutions to a single line to remove sensitive data"""
         line = LOG_JSON_RE.sub(b"'\\1': '<REMOVED>'", line)
         line = LOG_INI_HIDE_RE.sub(b"\\1 = <REMOVED>", line)
         line = LOG_NNTP_AUTH_RE.sub(b"\\1 <REMOVED>", line)
         line = LOG_HASH_RE.sub(b"<HASH>", line)
-        with suppress(ValueError):
-            prefix, rest = line.split(b"] ", 1)
-            rest = IPV6_RE.sub(b"<REMOVED>", rest)
-            rest = IPV4_RE.sub(b"<REMOVED>", rest)
-            line = prefix + b"] " + rest
+        line = LOG_REMOTE_LABEL_RE.sub(remote_label_replacement, line)
         if cur_user_bytes:
             line = line.replace(cur_user_bytes, b"<USERNAME>")
         return line
